be8558339526d311b00dc86245a0c2c965edf1d5
e <2485450596@qq.com>
2019-04-17 16:41:01 +0800

    增加游戏列表界面

--- src/views/game/list.vue
+++ src/views/game/list.vue
@@ -16,6 +16,138 @@
       </mu-button>
     </mu-appbar>
 
+
+
+    <mu-paper :z-depth="1" class="demo-list-wrap">
+      
+      <mu-list textline="three-line">
+        <mu-sub-header>--热门游戏--</mu-sub-header>
+        <mu-list-item avatar :ripple="false" button>
+          <mu-list-item-action>
+            <mu-avatar>
+              <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2653777330,1123369643&fm=27&gp=0.jpg">
+            </mu-avatar>
+          </mu-list-item-action>
+          <mu-list-item-content>
+            <mu-list-item-title>这个周末一起吃饭么?</mu-list-item-title>
+            <mu-list-item-sub-title>
+              <span style="color: rgba(0, 0, 0, .87)">Myron Liu -</span> 周末要来你这里出差，要不要一起吃个饭呀，实在编不下去了,哈哈哈哈哈哈
+            </mu-list-item-sub-title>
+          </mu-list-item-content>
+        </mu-list-item>
+        <mu-divider></mu-divider>
+        <mu-list-item avatar :ripple="false" button>
+          <mu-list-item-action>
+            <mu-avatar>
+              <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2599779665,4254172285&fm=27&gp=0.jpg">
+            </mu-avatar>
+          </mu-list-item-action>
+          <mu-list-item-content>
+            <mu-list-item-title>Alex Qin</mu-list-item-title>
+            <mu-list-item-sub-title>
+              <span style="color: rgba(0, 0, 0, .87)">看电影啊</span><br/>
+              我们去看电影，最近有部烂片上映，又有吐槽的了
+            </mu-list-item-sub-title>
+          </mu-list-item-content>
+        </mu-list-item>
+        <mu-divider></mu-divider>
+        <mu-list-item avatar :ripple="false" button>
+          <mu-list-item-action>
+            <mu-avatar>
+              <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2031496025,569053297&fm=27&gp=0.jpg">
+            </mu-avatar>
+          </mu-list-item-action>
+          <mu-list-item-content>
+            <mu-list-item-title>LOL</mu-list-item-title>
+            <mu-list-item-sub-title>
+              <span style="color: rgba(0, 0, 0, .87)">去打游戏啊</span><br/>
+              周末一起 LOL
+            </mu-list-item-sub-title>
+          </mu-list-item-content>
+        </mu-list-item>
+        <mu-divider></mu-divider>
+        <mu-list-item avatar :ripple="false" button>
+          <mu-list-item-action>
+            <mu-avatar>
+              <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2653982291,1045780277&fm=27&gp=0.jpg">
+            </mu-avatar>
+          </mu-list-item-action>
+          <mu-list-item-content>
+            <mu-list-item-title>LOL</mu-list-item-title>
+            <mu-list-item-sub-title>
+              <span style="color: rgba(0, 0, 0, .87)">哇去</span><br/>
+              实在编不下去，这就是个demo
+            </mu-list-item-sub-title>
+          </mu-list-item-content>
+        </mu-list-item>
+      </mu-list>
+
+      <mu-divider></mu-divider>
+
+      <mu-list textline="three-line">
+        <mu-sub-header>--全部游戏--</mu-sub-header>
+        <mu-list-item avatar :ripple="false" button>
+          <mu-list-item-action>
+            <mu-avatar>
+              <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2653777330,1123369643&fm=27&gp=0.jpg">
+            </mu-avatar>
+          </mu-list-item-action>
+          <mu-list-item-content>
+            <mu-list-item-title>这个周末一起吃饭么?</mu-list-item-title>
+            <mu-list-item-sub-title>
+              <span style="color: rgba(0, 0, 0, .87)">Myron Liu -</span> 周末要来你这里出差，要不要一起吃个饭呀，实在编不下去了,哈哈哈哈哈哈
+            </mu-list-item-sub-title>
+          </mu-list-item-content>
+        </mu-list-item>
+        <mu-divider></mu-divider>
+        <mu-list-item avatar :ripple="false" button>
+          <mu-list-item-action>
+            <mu-avatar>
+              <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2599779665,4254172285&fm=27&gp=0.jpg">
+            </mu-avatar>
+          </mu-list-item-action>
+          <mu-list-item-content>
+            <mu-list-item-title>Alex Qin</mu-list-item-title>
+            <mu-list-item-sub-title>
+              <span style="color: rgba(0, 0, 0, .87)">看电影啊</span><br/>
+              我们去看电影，最近有部烂片上映，又有吐槽的了
+            </mu-list-item-sub-title>
+          </mu-list-item-content>
+        </mu-list-item>
+        <mu-divider></mu-divider>
+        <mu-list-item avatar :ripple="false" button>
+          <mu-list-item-action>
+            <mu-avatar>
+              <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2031496025,569053297&fm=27&gp=0.jpg">
+            </mu-avatar>
+          </mu-list-item-action>
+          <mu-list-item-content>
+            <mu-list-item-title>LOL</mu-list-item-title>
+            <mu-list-item-sub-title>
+              <span style="color: rgba(0, 0, 0, .87)">去打游戏啊</span><br/>
+              周末一起 LOL
+            </mu-list-item-sub-title>
+          </mu-list-item-content>
+        </mu-list-item>
+        <mu-divider></mu-divider>
+        <mu-list-item avatar :ripple="false" button>
+          <mu-list-item-action>
+            <mu-avatar>
+              <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2653982291,1045780277&fm=27&gp=0.jpg">
+            </mu-avatar>
+          </mu-list-item-action>
+          <mu-list-item-content>
+            <mu-list-item-title>LOL</mu-list-item-title>
+            <mu-list-item-sub-title>
+              <span style="color: rgba(0, 0, 0, .87)">哇去</span><br/>
+              实在编不下去，这就是个demo
+            </mu-list-item-sub-title>
+          </mu-list-item-content>
+        </mu-list-item>
+      </mu-list>
+
+    </mu-paper>
+
   </div>  
 </template>
 
@@ -26,6 +158,9 @@ export default {
 </script>
 
 <style scoped>
-
+.demo-list-wrap {
+  width: 100%;
+  overflow: hidden;
+}
 </style>
 
