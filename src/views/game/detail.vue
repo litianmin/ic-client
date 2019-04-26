3cabff4cc414d90024207f80fe552a67d18652be
e <2485450596@qq.com>
2019-04-26 15:31:45 +0800

    整理样式

--- src/views/game/detail.vue
+++ src/views/game/detail.vue
@@ -6,7 +6,7 @@
         <mu-icon value="navigate_before"></mu-icon>
       </mu-button>
       
-      <div style=" font-size:14px;">
+      <div style="font-size:14px;">
         洛奇英雄传
       </div>
     </mu-appbar>
@@ -28,17 +28,18 @@
     <!-- END 轮播图 -->
 
     <!-- BEGIN 游戏简介 -->
-    <mu-row style="margin-top:.5rem; padding:0 .5rem; margin-bottom:.5rem;">
+    <mu-row class="game-brief-desc">
       <mu-col span="12">
         <div>
-          <span style="color:black; font-size:14px;">简介：</span> <span style="color:#9e9e9e; font-size:12px;">洛奇英雄传是我最喜欢的游戏哦，不管你喜不喜欢，反正我很喜欢啦，一款大型聊天站街游戏！</span>
+          <span style="color:black; font-size:14px;">简介：</span> 
+          <span style="color:#9e9e9e; font-size:12px;">洛奇英雄传是我最喜欢的游戏哦，不管你喜不喜欢，反正我很喜欢啦，一款大型聊天站街游戏！</span>
         </div> 
       </mu-col>
     </mu-row>
     <!-- END 游戏简介 -->
 
     <!-- BEGIN 游戏标签 -->
-    <div style="margin-top:.5rem; padding:0 .5rem;">
+    <div class="game-badge">
       <mu-badge content="端游" color="#00bcd4" style="margin-right:.5rem;"></mu-badge>
       <mu-badge content="角色扮演" color="#00bcd4" style="margin-right:.5rem;"></mu-badge>
       <mu-badge content="格斗" color="#00bcd4" style="margin-right:.5rem;"></mu-badge>
@@ -49,13 +50,14 @@
     <div style="margin-top:.8rem;">
       <mu-divider></mu-divider>
 
-      <mu-tabs :value.sync="active" inverse color="#80cbc4" indicator-color="#80cbc4" text-color="rgba(0, 0, 0, .54)"  full-width id="tabBar" :class="tabBarFixed == true ? 'isFixed' :''">
+      <!-- 内容切换条 -->
+      <mu-tabs :value.sync="active" inverse color="#80cbc4" indicator-color="#80cbc4" text-color="rgba(0, 0, 0, .54)"  full-width id="tabBar">
         <mu-tab><span>评 论</span></mu-tab>
         <mu-tab><span>组 队</span></mu-tab>
       </mu-tabs>
 
       <!-- BEGIN 评论 -->
-      <mu-container v-show="active === 0" style="margin-top:.5rem; padding:.5rem 1rem;">
+      <mu-container v-show="active === 0" class="comment-box">
         <!-- BEGIN 评论样式一 -->
         <div style="margin-bottom:2rem;">
           <mu-flex align-items="center">
@@ -121,27 +123,29 @@
             <mu-avatar size="28">
               <img src="http://img4.imgtn.bdimg.com/it/u=406799163,4023058385&fm=11&gp=0.jpg">
             </mu-avatar>
-            <span style="margin-left:.5rem; font-size:14px; color:#4db6ac;">
+            <span class="comment-item-nickname">
               瓦卡萨拉 
             </span>
-            <span style="margin-left: auto; font-size:12px; color:#9e9e9e;">2018-01-02 01:02:03</span>
+            <span class="comment-item-time">2018-01-02 01:02:03</span>
           </mu-flex>
 
-          <mu-row style="padding:.5rem 0 0 2rem; font-size:12px; color:#616161;">
+          <mu-row class="comment-item-text">
             我先来一个评论，然后出现这样那样的问题！
           </mu-row>
 
-          <mu-row style="padding:.5rem .5rem 0 2rem; font-size:12px; color:#616161;">
-            <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3758273131,3198344184&fm=27&gp=0.jpg" style="max-width:100%; max-height:100%;" alt="">
+          <mu-row class="comment-item-img">
+            <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3758273131,3198344184&fm=27&gp=0.jpg" alt="">
           </mu-row>
 
           <mu-row justify-content="end" style="padding:.5rem;">
               <mu-flex align-items="center">
-                <mu-icon value="thumb_up" size="12" color="#4db6ac"></mu-icon><span style="font-size:12px; margin-left:.2rem; color:#9e9e9e;">1000</span>
+                <mu-icon value="thumb_up" size="12" color="#4db6ac"></mu-icon>
+                <span class="comment-item-thumbup-count">1000</span>
               </mu-flex>
 
               <mu-flex align-items="center" style="margin-left:.8rem;">
-                <mu-icon value="comment" size="12" color="#4db6ac"></mu-icon><span style="font-size:12px; margin-left:.2rem; color:#9e9e9e;">2000</span>
+                <mu-icon value="comment" size="12" color="#4db6ac"></mu-icon>
+                <span class="comment-item-comment-count">2000</span>
               </mu-flex>
           </mu-row>
 
@@ -272,49 +276,50 @@
           <!-- END 组队样式二 -->
 
           <!-- BEGIN 组队样式三 -->
-          <mu-expansion-panel style="margin-bottom:1rem; border-radius:.5rem;">
-            <div slot="header" style="padding:.5rem 0 1rem 0;">
+          <mu-expansion-panel class="team-panel">
+            <div slot="header" class="team-header">
 
+              <!-- 队长基本信息(包括图片) -->
               <mu-row style="margin-top:.2rem;" gutter>
                 <!-- 左边内容 -->
                 <mu-col col="6" style="padding:.5rem 0 0 .5rem;">
-                  <mu-flex align-items="center" style="width:90%; margin-left:-.5rem;">
+                  <mu-flex align-items="center" class="team-avatar-flex">
                     <mu-avatar size="25">
                       <img src="http://img4.imgtn.bdimg.com/it/u=406799163,4023058385&fm=11&gp=0.jpg" />
                     </mu-avatar>
-                    <span style="margin-left:.5rem; font-size:14px; color:#795548;">风吹裤裆</span>
+                    <span class="team-leader-nickname">风吹裤裆</span>
                   </mu-flex>
 
-                  <div style="font-size:12px; margin-bottom:.2rem; margin-top:.5rem;">
-                    <span style="color:#795548;">角色：</span>
-                    <span style="color:#9e9e9e;">洛奇英雄传</span>
+                  <div class="team-leaderinfo-item">
+                    <span class="team-leaderinfo-title">角色：</span>
+                    <span class="team-leaderinfo-cont">洛奇英雄传</span>
                   </div>
-                  <div style="font-size:12px; margin-bottom:.2rem; margin-top:.5rem;">
-                    <span style="color:#795548;">游戏名称：</span>
-                    <span style="color:#9e9e9e;">我的名字</span>
+                  <div class="team-leaderinfo-item">
+                    <span class="team-leaderinfo-title">游戏名称：</span>
+                    <span class="team-leaderinfo-cont">我的名字</span>
                   </div>
-                  <div style="font-size:12px; margin-bottom:.2rem;">
-                    <span style="color:#795548;">区服：</span>
-                    <span style="color:#9e9e9e;">暗杀6区</span>
+                  <div class="team-leaderinfo-item">
+                    <span class="team-leaderinfo-title">区服：</span>
+                    <span class="team-leaderinfo-cont">暗杀6区</span>
                   </div>
-                  <div style="font-size:12px; margin-bottom:.2rem;">
-                    <span style="color:#795548;">等级/段位：</span>
-                    <span style="color:#9e9e9e;">66</span>
+                  <div class="team-leaderinfo-item">
+                    <span class="team-leaderinfo-title">等级/段位：</span>
+                    <span class="team-leaderinfo-cont">66</span>
                   </div>
-                  <div style="font-size:12px; margin-bottom:.2rem;">
-                    <span style="color:#795548;">招募人数：</span>
-                    <span style="color:#9e9e9e;">77</span>
+                  <div class="team-leaderinfo-item">
+                    <span class="team-leaderinfo-title">招募人数：</span>
+                    <span class="team-leaderinfo-cont">77</span>
                   </div>
-                  <div style="font-size:12px; margin-bottom:.2rem;">
-                    <span style="color:#795548;">队友偏向：</span>
-                    <span style="color:#9e9e9e;">男性/老油条/妹子</span>
+                  <div class="team-leaderinfo-item">
+                    <span class="team-leaderinfo-title">队友偏向：</span>
+                    <span class="team-leaderinfo-cont">男性/老油条/妹子</span>
                   </div>
                 </mu-col>  
 
                 <!-- 右边图片展示 -->
                 <mu-col col="6" justify-content="center" align-items="center">
-                  <mu-flex justify-content="center" align-items="center" style="width:100%; height:10rem; text-align:center; border-radius:.5rem; margin-top:.5rem; ">
-                    <img style="max-width:100%; max-height:10rem; border-radius:.5rem;" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556172407865&di=6171b22f5c8d59e11d35faef8afe7d54&imgtype=0&src=http%3A%2F%2Fimgsa.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D4332c8a0dd54564ee565e43983df9cde%2Fb812c8fcc3cec3fdf66553c6d688d43f8794272d.jpg" />
+                  <mu-flex justify-content="center" align-items="center" class="team-leaderinfo-img-flex">
+                    <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556172407865&di=6171b22f5c8d59e11d35faef8afe7d54&imgtype=0&src=http%3A%2F%2Fimgsa.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D4332c8a0dd54564ee565e43983df9cde%2Fb812c8fcc3cec3fdf66553c6d688d43f8794272d.jpg" />
                   </mu-flex>
                 </mu-col>
               </mu-row>
@@ -328,10 +333,11 @@
               </mu-row>
 
               <!-- 额外内容 -->
-              <mu-flex>
+              <mu-flex style="margin-top:.5rem;">
                 <div style="margin-left:auto;">
-                  <span style="font-size:12px; margin-right:1rem; color:#9e9e9e;">2分钟前</span>
-                  <span style="font-size:12px; color:#009688;">进入聊天</span>
+                  <span class="team-extracont-time">2分钟前</span>
+                  <!-- <span style="font-size:12px; color:#009688;">进入聊天</span> -->
+                  <span class="team-extracont-operate">加入组队</span>
                 </div>
               </mu-flex>
 
@@ -389,7 +395,7 @@
                 </mu-col>
               </mu-row>
 
-              <mu-row style="margin-bottom:.5rem; background:rgba(230, 230, 230, .5); padding:.4rem 0 .4rem .5rem; border-radius:.5rem;">
+              <mu-row class="teammate-box">
                 <mu-col span="9">
                   <mu-flex style="height:4rem;">
                     <mu-avatar size="28">
@@ -397,22 +403,21 @@
                     </mu-avatar>
 
                     <div>
-                      <span style="margin-left:.5rem; font-size:12px; color:#795548;">昵称：<span style="color:#9e9e9e;">风吹裤裆</span></span>
+                      <span class="teammate-info-title">昵称：<span style="color:#9e9e9e;">风吹裤裆</span></span>
                       <br/>
-                      <span style="margin-left:.5rem; font-size:12px; color:#795548;">角色名：<span style="color:#9e9e9e;">我是你的神</span></span>
+                      <span class="teammate-info-title">角色名：<span style="color:#9e9e9e;">我是你的神</span></span>
                       <br/>
-                      <span style="margin-left:.5rem; font-size:12px; color:#795548;">亦非/69级/大叔</span>
+                      <span class="teammate-info-title">亦非/69级/大叔</span>
                     </div>
                   </mu-flex>
                 </mu-col>
 
                 <mu-col span="3" justify-content="center">
-                  <mu-flex justify-content="center" align-items="center" style="width:100%; height:4rem; text-align:center; border-radius:.5rem; ">
-                      <img style="max-width:4rem; max-height:4rem; border-radius:.2rem;" src="http://pic33.photophoto.cn/20141204/0005018330800906_b.jpg" />
+                  <mu-flex justify-content="center" align-items="center" class="teammate-img-flex">
+                      <img src="http://pic33.photophoto.cn/20141204/0005018330800906_b.jpg" />
                   </mu-flex>
                 </mu-col>
               </mu-row>
-              
             </div>
 
           </mu-expansion-panel>
@@ -462,7 +467,6 @@
       ],
 
       active: 0,
-      tabBarFixed: false,   // 评论和组队导航栏是否固定
 
       panel: ''
 
@@ -476,28 +480,12 @@
         swiperTop.controller.control = swiperThumbs
         swiperThumbs.controller.control = swiperTop
       })
-
-      // window.addEventListener('scroll', this.handleScroll)
     },
 
     methods: {
-      // handleScroll () { // 处理评论、组队消息栏目
-      //   let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-      //   let offsetTop = document.querySelector('#tabBar').offsetTop
-
-      //   if (scrollTop > offsetTop) {
-      //     this.tabBarFixed = true
-      //   } else {
-      //     this.tabBarFixed = false
-      //   }
-      // },
       toggle (panel) {
         this.panel = panel === this.panel ? '' : panel;
       }
-    },
-
-    destroyed () {
-      // window.removeEventListener('scroll', this.handleScroll)
     },
   }
 </script>
@@ -510,13 +498,39 @@
 .swiper-slide { background-size: cover; background-position: center; }
 .swiper-img { max-width:100%; height:auto; border-bottom-left-radius:3px; border-bottom-right-radius:5px; }
 
-
 .gallery-top { height: 80%!important; width: 100%; margin-top:1px; }
 .gallery-thumbs { height: 20%!important; box-sizing: border-box; padding: 1px 0; }
 .gallery-thumbs .swiper-slide { width: 25%; height: 100%; opacity: 0.4; }
 .gallery-thumbs .swiper-slide-active { opacity: 1; }
 
- .isFixed{ position:fixed; background-color:#Fff; top:0; z-index:999; }
+.game-brief-desc { margin-top:.5rem; padding:0 .5rem; margin-bottom:.5rem; }
+.game-badge { margin-top:.5rem; padding:0 .5rem; }
+
+.comment-box { margin-top:.5rem; padding:.5rem 1rem; }
+.comment-item-nickname { margin-left:.5rem; font-size:14px; color:#4db6ac; }
+.comment-item-time { margin-left: auto; font-size:12px; color:#9e9e9e; }
+.comment-item-text { padding:.5rem 0 0 2rem; font-size:12px; color:#616161; }
+.comment-item-img { padding:.5rem .5rem 0 2rem; font-size:12px; color:#616161; }
+.comment-item-img img { max-width:100%; max-height:100%; border-radius:.3rem; }
+.comment-item-thumbup-count { font-size:12px; margin-left:.2rem; color:#9e9e9e; }
+.comment-item-comment-count { font-size:12px; margin-left:.2rem; color:#9e9e9e; }
+
+.team-panel { margin-bottom:1rem; border-radius:.5rem; }
+.team-header { padding:.5rem 0 .5rem 0; }
+.team-avatar-flex { width:90%; margin-left:-.5rem; margin-bottom:.5rem; }
+.team-leader-nickname { margin-left:.5rem; font-size:14px; color:#795548; }
+.team-leaderinfo-item { font-size:12px; margin-bottom:.2rem; }
+.team-leaderinfo-title { color:#795548; }
+.team-leaderinfo-cont { color:#9e9e9e; }
+.team-leaderinfo-img-flex { width:100%; height:10rem; text-align:center; border-radius:.5rem; margin-top:.5rem; }
+.team-leaderinfo-img-flex img { max-width:100%; max-height:10rem; border-radius:.5rem; }
+.team-extracont-time { font-size:12px; margin-right:1rem; color:#9e9e9e; }
+.team-extracont-operate { font-size:12px; color:#009688; }
+
+.teammate-box { margin-bottom:.5rem; background:rgba(230, 230, 230, .5); padding:.4rem 0 .4rem .5rem; border-radius:.5rem; }
+.teammate-info-title { margin-left:.5rem; font-size:12px; color:#795548; }
+.teammate-img-flex { width:100%; height:4rem; text-align:center; border-radius:.5rem; }
+.teammate-img-flex img { max-width:4rem; max-height:4rem; border-radius:.2rem; }
 
 .mu-expansion-toggle-btn { display: none; }
 
