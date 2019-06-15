e6402bd59e1adef5571b83cab5435f731f44b05d
e <2485450596@qq.com>
2019-06-15 11:51:51 +0800

    something new

--- src/views/travel/newTeam.vue
+++ src/views/travel/newTeam.vue
@@ -79,21 +79,20 @@
     </mu-container>
 
     <!-- 路线、活动内容 -->
-    <mu-container style="padding:0 .8rem 0 0;">
+    <mu-container style="padding:0 .8rem 0 0; margin-bottom:1rem;">
       <span style="font-size:12px; color:#795548; margin-right:.5rem; margin-left:.5rem; ">路线/活动内容：</span>
       <div class="demo-vsteper-container">
-        <mu-stepper :active-step="vactiveStep" orientation="vertical">
+        <mu-stepper :active-step="VactiveStep" orientation="vertical" style="margin-bottom:1rem;">
 
           <mu-step v-for="(item, index) in StepList" :key="index">
             <mu-step-label>
-              {{ item.meetingVenue }}
+              地点/活动 <span style="font-size:12px; color:#4caf50;" v-if="item.meetingVenue.length > 0">（{{ item.meetingVenue }}）</span>
             </mu-step-label>
             <mu-step-content>
 
               <mu-flex style="width:100%; margin-bottom:.5rem; margin-top:.5rem;" align-items="center">
                 <span style="font-size:12px; color:#795548; margin-right:.5rem;">集合地点：</span>
-                <span style="font-size:12px; color:#9e9e9e;">{{ item.meetingVenue }}</span>
-                <mu-icon value="person_pin_circle" size="20" color="#009688"></mu-icon>
+                  <input maxlength="15" v-model="item.meetingVenue" style="padding:.4rem; width:70%; font-size:12px; border-radius:.2rem; border:1px solid #e0e0e0; color:#212121;" type="text" placeholder="（不超过20个字符）">
               </mu-flex>
 
               <mu-row style="width:100%; margin-top:.5rem;" align-items="end">
@@ -106,45 +105,38 @@
                 </textarea>
               </mu-flex>
 
-              <mu-flex style="width:50%; height:4rem; padding:1rem; position:relative; border:1px solid #e0e0e0; border-radius:.3rem;" justify-content="center" align-items="center">
-                  <mu-icon value="add" size="40" color="#e0e0e0" style=""></mu-icon>
+              <!-- 添加图片 -->
+              <mu-flex style="width:50%; height:4rem; padding:.5rem; position:relative; border:1px solid #e0e0e0; border-radius:.3rem;" justify-content="center" align-items="center">
+                  <mu-icon v-show="item.descImg.length > 0" @click="delImg(index)" value="cancel" color="#f06292" style="position:absolute; top:-.8rem; right:-.8rem;"></mu-icon>
+                  <mu-icon @click="addImg(index)" v-show="item.descImg.length == 0" value="add" size="40" color="#e0e0e0" style=""></mu-icon>
+                  <img style="max-width:100%; max-height:4rem; border-radius:.3rem;" :src="item.descImg" alt="">
               </mu-flex>
 
-              <mu-button class="demo-step-button" @click="vhandleNext" color="primary">下一步</mu-button>
-              <mu-button class="demo-step-button" @click="vhandleNext" color="primary">完成</mu-button>
+              <mu-button v-if="index > 0" class="demo-step-button" @click="vhandlePrev" color="#00bcd4" small>上一个</mu-button>
+              <mu-button class="demo-step-button" @click="vhandleNext(index)" color="#00bcd4" small>下一个</mu-button>
+              <mu-button class="demo-step-button" @click="vhandleFinished" color="#00bcd4" small>完成</mu-button>
             </mu-step-content>
           </mu-step>
 
-          <mu-step>
-            <mu-step-label>
-              创建一个群组
-            </mu-step-label>
-            <mu-step-content>
-              <p>
-                创建群组，50人左右，以18-25单身青年为主。。。。。
-              </p>
-              <mu-button class="demo-step-button" @click="vhandleNext" color="primary">下一步</mu-button>
-              <mu-button flat class="demo-step-button" @click="vhandlePrev">上一步</mu-button>
-            </mu-step-content>
-          </mu-step>
-
-          <mu-step>
-            <mu-step-label>
-              宣传活动
-            </mu-step-label>
-            <mu-step-content>
-              <p>
-                多在群里发消息宣传宣传，有事没事多在群里唠唠嗑，确定的话就ok拉
-              </p>
-              <mu-button class="demo-step-button" @click="vhandleNext" color="primary">完成</mu-button>
-              <mu-button flat class="demo-step-button" @click="vhandlePrev">上一步</mu-button>
-            </mu-step-content>
-          </mu-step>
         </mu-stepper>
-        <p v-if="vfinished">
-          都完成啦!<a href="javascript:;" @click="vreset">点这里</a>可以重置
-        </p>
+
+        <input @change="getImgList" ref="imgUpload" type="file" style="display:none;" multiple accept="image/*"/>
+
+        <mu-flex v-if="vfinished" justify-content="start" style="padding: 0 0 0 2.5rem;">
+          <span>
+            已完成！<span style="color:#009688;" @click="vreset">点击重置</span>
+          </span>
+        </mu-flex>
+        
+        <!-- 提交按钮 -->
+        <mu-flex align-items="center" justify-content="center" style=" width:100%; margin-top:1.5rem;">
+          <mu-button style="width:100%;" color="#03a9f4">
+            发起组队
+          </mu-button>
+        </mu-flex>
       </div>
+
+
     </mu-container>
 
 
@@ -155,7 +147,7 @@
 export default {
   data () {
     return {
-      vactiveStep: 0,
+      VactiveStep: 0,
       TravelTheme: 1,
       PathLength: 1,
       TravelType: 1,
@@ -166,12 +158,11 @@ export default {
       MeetingVenue: '', // 聚会地点
       MeetingTime: new Date, // 活动开始时间
       StepList: [
-        {meetingVenue: '地点/路线/时间', beginTime: new Date, endTime: new Date, travelDesc: '这里是我的描述'},
-        {meetingVenue: '地点/路线/时间', beginTime: new Date, endTime: new Date, travelDesc: '这里是我的描述'},
-        {meetingVenue: '地点/路线/时间', beginTime: new Date, endTime: new Date, travelDesc: '这里是我的描述'},
-        {meetingVenue: '地点/路线/时间', beginTime: new Date, endTime: new Date, travelDesc: '这里是我的描述'},
-        {meetingVenue: '地点/路线/时间', beginTime: new Date, endTime: new Date, travelDesc: '这里是我的描述'},
+        {meetingVenue: '', beginTime: new Date, endTime: new Date, travelDesc: '', descImg: ''},
+        {meetingVenue: '', beginTime: new Date, endTime: new Date, travelDesc: '', descImg: ''},
       ],
+      HadAddImg: 0,
+      ClickAddImgIndex: 0,
     }
   },
   mounted () {
@@ -181,18 +172,51 @@ export default {
   },
   computed: {
     vfinished () {
-      return this.vactiveStep > 2
+      return this.VactiveStep == (this.StepList.length + 1)
     }
   },
   methods: {
-    vhandleNext () {
-      this.vactiveStep++
+    vhandleNext (index) {  // 步骤下一步
+      // 往StepList添加数据
+      if(index == this.StepList.length - 1) {
+        let item = {meetingVenue: '', beginTime: undefined, endTime: undefined, travelDesc: '', descImg: ''}
+        this.StepList.push(item)
+      }
+      this.VactiveStep++
     },
-    vhandlePrev () {
-      this.vactiveStep--
+    vhandlePrev () {  // 步骤上一步
+      this.VactiveStep--
     },
-    vreset () {
-      this.vactiveStep = 0
+    vreset () { // 步数重置
+      this.VactiveStep = 0
+    },
+    vhandleFinished () {
+      this.VactiveStep = this.StepList.length + 1
+    },
+    addImg (index) {
+      this.ClickAddImgIndex = index
+      this.$refs.imgUpload.click()
+    },
+    getImgList () {  // 获取评论图片
+      var _this = this
+      var event = event || window.event
+      var file = event.target.files[0]
+      if(file.size > 1024 * 1024 * 2) {
+        this.$toast.message('图片上传最大为2M')
+        return
+      }
+      var reader = new FileReader()
+      //转base64
+      reader.onload = function(e) {
+        let imgIndex = _this.ClickAddImgIndex
+        _this.StepList[imgIndex].descImg = e.target.result
+        _this.ClickAddImgIndex++
+      }
+      reader.readAsDataURL(file)
+    },
+    delImg (index) {
+      this.StepList[index].descImg = ''
+      this.ClickAddImgIndex--
     },
     goBack () {
       this.$router.go(-1)
@@ -215,8 +239,9 @@ export default {
 }
 
 .demo-step-button {
-  margin-top: 12px;
-  margin-right: 12px;
+  font-size: 12px;
+  margin-top: 1rem;
+  margin-right: .5rem;
 }
 </style>
 
