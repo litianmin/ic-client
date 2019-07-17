1da46bebd48c49343a237196c437e1ec73992482
“litianmin <l>
2019-07-17 23:19:19 +0800

    something new

--- src/views/instant/detail.vue
+++ src/views/instant/detail.vue
@@ -40,7 +40,86 @@
       <mu-row class="comment-item-img">
         <img src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2458914123,541076108&fm=26&gp=0.jpg" alt="">
       </mu-row>
+
+      <mu-row style="padding:1rem .5rem;" >
+        <mu-icon value="person_pin_circle" color="#4caf50" size="20"></mu-icon>
+        <span style="font-size:12px; color:#9e9e9e;">地点：我的地点 · 距离你50m</span>
+      </mu-row>
     </div>
+
+
+    <!-- BEGIN 队长和队友列表 -->
+    <mu-flex style="padding:.5rem 1rem; background:#fff;" justify-content="center" align-items="center" wrap="wrap">
+        <mu-avatar v-for="(item, index) in TeammateList" :key="index" size="35" :class="item.sex == 1 ? 'avatar-male' : 'avatar-female'" style="margin-right:.5rem;">
+          <img :src="item.avatar" alt="">
+        </mu-avatar>
+        <span v-if="TeamBaseInfo.recruitStatus == 0" @click="joinTeam">
+          <svg-icon icon-class="add_circle_outline" style="font-size:40px; color:red;"></svg-icon>
+        </span>
+    </mu-flex>
+    <mu-flex justify-content="center" style="padding:.3rem 0 .5rem 0; border-bottom:1px dashed #e0e0e0; background:#fff;">
+      <span style="font-size:12px; color:#9e9e9e;">-- 招募{{ TeamBaseInfo.recruitNumb }}人，还差{{ TeamBaseInfo.recruitNumb - TeamBaseInfo.hadRecruitNumb }}人 --</span>
+    </mu-flex>
+    <!-- END 队长和队友列表 -->
+
+    <!-- BEGIN 排序条 -->
+    <mu-flex class="sort-bar" justify-content="center" align-items="center" >
+      <span style="margin-left:.3rem">回复列表 (0)</span>
+      <span @click="convertSort" class="sort-bar-svg"><svg-icon :icon-class="IsSortup == true ? 'sortup' : 'sortdown'"></svg-icon></span>
+    </mu-flex>
+    <!-- END 排序条 -->
+
+    <!-- BEGIN 回复评论 -->
+    <mu-load-more :loading="Loading" @load="load" :loaded-all="IsTheLast">
+      <mu-container class="reply-container" v-for="(item, index) in ReplyList" :key="index">
+        <mu-flex align-items="center">
+          <mu-avatar size="24" :class="item.user_sex == 1 ? 'avatar-male' : 'avatar-female'">
+            <img :src="item.user_avatar">
+          </mu-avatar>
+          <span class="reply-nickname">
+            {{ item.user_nickname }} 
+          </span>
+          <span class="reply-time">{{ item.create_time }}</span>
+        </mu-flex>
+
+        <mu-row class="reply-cont-box">
+          <span style="font-size:12px; margin-left:.5rem; ">
+            <span v-if="item.reply_to > 0">@<span style="color:#795548;">{{ item.reply_nickname }}</span> :</span> {{ item.chat_cont }}
+            <span style="color:green; margin-left:.5rem;" @click="newChat(true, item.user_id, item.user_nickname)">回复</span>
+          </span>
+        </mu-row>
+
+        <mu-row v-if="item.chat_img" class="team-item-img" style="padding:.5rem .5rem .5rem 1rem; ">
+          <img :src="item.chat_img">
+        </mu-row>
+      </mu-container>
+    </mu-load-more>
+
+    <mu-row v-show="IsTheLast" justify-content="center" style="padding:.5rem .5rem .3rem .5rem; margin-top:.3rem; margin-bottom:3rem; color:#9e9e9e;">
+      <span> 没有更多的回复 </span>
+    </mu-row>
+    <!-- END 回复评论 -->
+
+    <!-- 发起评论框 -->
+    <mu-flex class="reply-input-box" align-items="center">
+      <div style="width:80%;" @click="newChat(false, 0, '')">
+        <input type="text" placeholder="我也来说一句吧" disabled>
+      </div>
+
+      <span v-if="JointeamStmt == 1 || JointeamStmt == 2 || JointeamStmt == 5" @click="joinTeam" style="font-size:19px; margin-left:auto;">
+        <svg-icon icon-class="jointeam_refuse"></svg-icon>
+      </span>
+      <span v-if="JointeamStmt == 3" @click="joinTeam" style="font-size:19px; margin-left:auto;">
+        <svg-icon icon-class="hadjointeam"></svg-icon>
+      </span>
+      <!-- <span v-if="JointeamStmt == 1" @click="joinTeam" style="font-size:20px; margin-left:auto;">
+        <svg-icon icon-class="jointeam_applying"></svg-icon>
+      </span> -->
+      <span v-if="JointeamStmt == 0 || JointeamStmt == 4" @click="joinTeam" style="font-size:20px; margin-left:auto;">
+        <svg-icon icon-class="jointeam"></svg-icon>
+      </span>
+      <mu-icon value="share" class="reply-input-box-icon" size="18" color="#8A8A8A"></mu-icon>
+    </mu-flex>
 
   </div>
 </template>
@@ -50,6 +129,36 @@ export default {
   data () {
     return {
       JointeamStmt: 0,
+
+      InitLoading: true,
+      TeamID: 0,
+      IsSortup: false,
+      IsTheLast: true,
+      ReplyListPage: 1,
+      ReplyList: [],
+      Loading: false,
+      RecruitImgs: [],
+      SwiperIsRender: false,
+      TeamBaseInfo: {
+        partyVenue: {
+          addr: '',
+          lat: 0,
+          lng: 0,
+          name: ''
+        },
+        meetingVenue: {
+          addr: '',
+          lat: 0,
+          lng: 0,
+          name: ''
+        },
+        recruitStatus: 0, // 0=>组队中， 1=>停止招募(招募成功或者已过期), 2=>已解散(只有组队中才能解散，停止招募后不能解散)
+        hadRecruitNumb: 0,
+      },
+      TeammateList: [],
+      JointeamStmt: 0,
+      IsCaptain: false,
+
     }
   },
   methods: {
@@ -57,8 +166,130 @@ export default {
       this.$router.go(-1)
     },
 
-    leaveTeam () {
+    load () {
+      this.Loading = true      
+      let sortWay = this.IsSortup == false ? 0 : 1
+      this.$axios.post(`/party/chatList/${this.ReplyListPage}/${this.TeamID}/${sortWay}`,{}).then((resp)=>{
+        if(resp.data.code != 20000) {
+          this.$toast.message(resp.data.msg)
+          return
+        }
 
+        let dataBack = resp.data.msg
+        this.IsTheLast = dataBack.isTheLast
+        let replyList = dataBack.listInfo
+        for(let i = 0; i < replyList.length; i++) {
+          replyList[i].create_time = utils.getDateDiff(replyList[i].create_time, true)
+          replyList[i].user_avatar = utils.imgPrefixDeal(replyList[i].user_avatar)
+          replyList[i].chat_img = utils.imgPrefixDeal(replyList[i].chat_img)
+        }
+        this.ReplyList = this.ReplyList.concat(replyList)
+        this.ReplyListPage++
+        this.Loading = false
+      })
+    },
+    convertSort () {
+      // 当转换排序顺序的时候，把页数重置，然后，重新加载回复评论
+      this.ReplyListPage = 1
+      this.ReplyList = []
+      this.IsSortup = !this.IsSortup
+      this.load()
+    },
+    replytoComment (isReply, replyID, replyNickname) {
+      this.$router.push({path:`/game/replytoComment`, query:{commentID:this.CommentID, isReply:isReply, replyID:replyID, replyNickname:replyNickname}})
+    },
+    joinTeam () {
+      // 首先判断队伍的状态是否停止招募
+      if(this.TeamBaseInfo.recruitStatus > 0) {
+        switch(this.TeamBaseInfo.recruitStatus) {
+          case 1:
+            this.$toast.message('该队伍已停止招募，不能加入组队')
+          break
+          case 2:
+            this.$toast.message('该队伍已解散，不能加入组队')
+          break
+        }
+        return
+      }
+
+      // 点击加入组队的时候，判断加入组队的状态
+      // 0 => 未加入，1=>申请，2=>拒绝加入，3=>已加入，4=>离队, 5=>被踢
+      // 因为party里面没有1、2、5状态， 只需要判断 0 、3、 4(暂时不做其他的)
+      switch(this.JointeamStmt) {
+        case 0: // 未加入
+          this.$axios.post(
+            `/party/joinTeam/${this.TeamID}`,{}
+          ).then((resp)=>{
+            let dataBack = resp.data
+            this.$toast.message(dataBack.msg)
+            // window.location.reload()
+          })
+        break
+        case 1: // 申请
+          // 不做处理
+        break
+        case 2: // 拒绝加入
+          // 不做处理
+        break
+        case 3: // 已加入
+          // 不做处理
+          if(this.IsCaptain == true) {
+            this.$toast.message('你是队长，你要邀请别人进来吗？')
+          }else{
+            this.$toast.message('你已加入组队！')
+          }
+        break
+        case 4: // 已离队，重新加入
+          this.$axios.post(
+            `/party/joinTeam/${this.TeamID}`,{}
+          ).then((resp)=>{
+            let dataBack = resp.data
+            this.$toast.message(dataBack.msg)
+            if(dataBack.code == 20000) {
+              window.location.reload()
+              console.log('准备更新工作')
+            }
+          })
+        break
+        case 5:
+          // 不做处理
+        break
+      }
+    },
+    leaveTeam () {
+      // 先判断是否为队长，如果是队长，提示会解散队伍
+      // 并且如果是队长，只有组队中才可以解散
+      if(this.IsCaptain == true) {
+        this.$confirm('是否解散队伍？').then((resp)=>{
+          if(resp.result == true) { // 确定解散队伍
+            // this.$toast.message('你确定解散了队伍')
+            this.$axios.post(`/party/leaveTeam/${this.TeamID}`, {}).then((resp)=>{
+              if(resp.data.code == 20000) {
+                this.$toast.message('已成功解散')
+                this.$router.push(`/party/list`)
+                return
+              }
+            }) 
+          }
+        })
+      } else {
+        this.$axios.post(`/party/leaveTeam/${this.TeamID}`, {}).then((resp)=>{
+          console.log(resp)
+          if(resp.data.code == 20000) {
+            this.$toast.message('已退出队伍')
+            this.$router.push(`/party/list`)
+            return
+          }
+        }) 
+      }
+    },
+    newChat (isReply, replyID, replyNickname) {
+      // 只有加入组队的人才能进行评论
+      if(this.JointeamStmt != 3) {
+        this.$toast.message('加入组队后才能聊天哦')
+        return
+      }
+      this.$router.push({path:`/party/newChat`, query:{teamID:this.TeamID, isReply:isReply, replyID:replyID, replyNickname:replyNickname}})
     },
 
   }
@@ -71,6 +302,22 @@ export default {
 .mine-menu-list { background:#4dd0e1; color:white; padding:0; }
 .mine-menu-item { color:#fff; font-size:12px; }
 
+
+.sort-bar { background:#f5f5f5; border-top:1px solid #fff; font-size:12px; padding:.4rem .5rem; margin-bottom:.5rem; }
+.sort-bar-svg { margin-left:auto; font-size:18px; margin-right:.3rem; }
+
+.reply-container { padding:.5rem .8rem; border-bottom:.1rem solid #ffffff; }
+
+.reply-nickname { font-size:12px; margin-left:.5rem; margin-right:.5rem; color:#4db6ac; }
+.reply-time { margin-left:auto; font-size:12px; color:#9e9e9e; margin-right:.5rem; }
+.reply-cont-box { font-size:12px; padding:.5rem .5rem .5rem 1rem; }
+
+.reply-input-box { position:fixed; bottom:0; width:100%; padding:.5rem; background:#ffffff; border-top:1px solid #e0e0e0; }
+.reply-input-box input { width:100%; border-radius:.3rem; padding:.3rem; background:#f5f5f5; border:0; font-size:12px; }
+.reply-input-box-span { margin-left:auto; font-size:18px; margin-right:.3rem; }
+.reply-input-box-icon { margin-left:auto; margin-right:.5rem; }
+
+
 .comment-box { padding:.5rem 1rem; }
 .comment-item-nickname { margin-left:.5rem; font-size:12px; color:#4db6ac; }
 .comment-item-time { margin-left: auto; margin-right:.3rem; font-size:12px; color:#9e9e9e; }
@@ -79,6 +326,10 @@ export default {
 .comment-item-img img { max-width:100%; max-height:100%; border-radius:.3rem; }
 .comment-item-thumbup-count { font-size:12px; margin-left:.2rem; color:#9e9e9e; }
 .comment-item-comment-count { font-size:12px; margin-left:.2rem; color:#9e9e9e; }
+
+.avatar-male { padding:.1rem; border:1px solid #bbdefb; border-radius:50%; background:white; }
+.avatar-female { padding:.1rem; border:1px solid #f8bbd0; border-radius:50%; background:white; }
+
 </style>
 
 
