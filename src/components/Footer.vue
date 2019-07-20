bceb51cb0a77347a5ceb483c0baec63e67f998f2
e <2485450596@qq.com>
2019-07-20 14:14:36 +0800

    something new

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -21,11 +21,12 @@
           <span :class="navActive == 'nearby' ? 'font-active' : ''">附近</span>
         </div>
       </mu-flex>
-      <mu-flex @click="linkToNav(2)" justify-content="center" align-items="center" wrap="wrap">
+      <mu-flex @click="linkToNav(2)" justify-content="center" align-items="center" wrap="wrap" style="position:relative;">
         <svg-icon :icon-class="navActive == 'msg' ? 'msg_focus' : 'msg_blur'" class="icon-class" style="font-size:18px;"></svg-icon>
         <div style="width:100%; text-align:center; margin-top:.1rem; font-size:12px;">
           <span :class="navActive == 'msg' ? 'font-active' : ''">消息</span>
         </div>
+        <div style="position:absolute; top:0; left:0; width:.5rem; height:.5rem; border-radius:50%; background:red;"></div>
       </mu-flex>
       <mu-flex @click="linkToNav(3)" justify-content="center" align-items="center" wrap="wrap">
         <svg-icon :icon-class="navActive == 'personnalMsg' ? 'personal_msg_focus' : 'personal_msg_blur'" class="icon-class" style="font-size:18px;"></svg-icon>
@@ -54,6 +55,7 @@ export default {
       switch(nav) {
         case 0:
           this.navActive = 'mainPage'
+          this.$router.push('/')
         break
         case 1:
           this.navActive = 'nearby'
--- src/views/notify/category.vue
+++ src/views/notify/category.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- BEGIN 头部 -->
-    <mu-appbar class="mine-appbar" color="#607d8b" z-depth="1">
+    <mu-appbar class="mine-appbar" color="#00bcd4" z-depth="1">
       <mu-button icon slot="left">
         <mu-icon value="navigate_before"></mu-icon>
       </mu-button>
@@ -13,15 +13,25 @@
     <!-- END 头部 -->
 
     <div style="padding:1rem;">
-      <mu-row style="margin-top:.5rem; margin-bottom:1rem;">
+      <mu-row style="margin-top:1rem; margin-bottom:1rem;">
         <mu-col span="3">
-          <mu-flex style="width:100%; height:4rem; background:red; color:#fff; border-radius:1rem;" align-items="center" justify-content="center">
-            <span>组 队</span>
+          <mu-flex style="width:100%; height:auto; color:#fff;" align-items="center" >
+              <div style="width:3.8rem; height:3.8rem; background:#03a9f4; color:#fff; padding:.5rem; border-radius:50%;">
+                <div style="text-align:center;">
+                  <svg-icon icon-class="notify_team" class="icon-class"></svg-icon>
+                </div>
+                <div style="text-align:center; font-size:13px;">组 队</div>
+              </div>
           </mu-flex>
         </mu-col>
-        <mu-col span="9">
-          <mu-row>标题一</mu-row>
-          <mu-row>这里是我的详细内容，不要问，不要说，就静静的听我说，我真的是</mu-row>
+        <mu-col span="9" align-self="center">
+          <mu-row>
+            <span style="font-size:18px; font-weight:700;">组队成功</span> 
+            <span style="margin-left:auto; font-size:14px; color:#9e9e9e; margin-right:.5rem;">10分钟前</span>
+          </mu-row>
+          <mu-row style="margin-top:.2rem;">
+            <span style="color:#757575; font-size:16px;">这里是我的详细内容，不要问，</span>
+          </mu-row>
         </mu-col>
       </mu-row>
       <mu-divider></mu-divider>
@@ -29,69 +39,121 @@
 
       <mu-row style="margin-top:1rem; margin-bottom:1rem;">
         <mu-col span="3">
-          <mu-flex style="width:100%; height:4rem; background:red; color:#fff; border-radius:1rem;" align-items="center" justify-content="center">
-            <span>谁@我</span>
+          <mu-flex style="width:100%; height:auto; color:#fff;" align-items="center" >
+              <div style="width:3.8rem; height:3.8rem; background:#00bcd4; color:#fff; padding:.5rem; border-radius:50%;">
+                <div style="text-align:center;">
+                  <svg-icon icon-class="notify_sms" class="icon-class"></svg-icon>
+                </div>
+                <div style="text-align:center; font-size:13px;">@ 你</div>
+              </div>
           </mu-flex>
         </mu-col>
-        <mu-col span="9">
-          <mu-row>标题一</mu-row>
-          <mu-row>这里是我的详细内容，不要问，不要说，就静静的听我说，我真的是</mu-row>
+        <mu-col span="9" align-self="center">
+          <mu-row>
+            <span style="font-size:18px; font-weight:700;">系统</span> 
+            <span style="margin-left:auto; font-size:14px; color:#9e9e9e; margin-right:.5rem;">10分钟前</span>
+          </mu-row>
+          <mu-row style="margin-top:.2rem;">
+            <span style="color:#757575; font-size:16px;">这里是我的详细内容，不要问，</span>
+          </mu-row>
         </mu-col>
       </mu-row>
       <mu-divider></mu-divider>
 
       <mu-row style="margin-top:1rem; margin-bottom:1rem;">
         <mu-col span="3">
-          <mu-flex style="width:100%; height:4rem; background:red; color:#fff; border-radius:1rem;" align-items="center" justify-content="center">
-            <span>系统通知</span>
+          <mu-flex style="width:100%; height:auto; color:#fff;" align-items="center" >
+              <div style="width:3.8rem; height:3.8rem; background:#009688; color:#fff; padding:.5rem; border-radius:50%;">
+                <div style="text-align:center;">
+                  <svg-icon icon-class="notify_member" class="icon-class"></svg-icon>
+                </div>
+                <div style="text-align:center; font-size:13px;">会 员</div>
+              </div>
           </mu-flex>
         </mu-col>
-        <mu-col span="9">
-          <mu-row>标题一</mu-row>
-          <mu-row>这里是我的详细内容，不要问，不要说，就静静的听我说，我真的是</mu-row>
+        <mu-col span="9" align-self="center">
+          <mu-row>
+            <span style="font-size:18px; font-weight:700;">组队成功</span> 
+            <span style="margin-left:auto; font-size:14px; color:#9e9e9e; margin-right:.5rem;">10分钟前</span>
+          </mu-row>
+          <mu-row style="margin-top:.2rem;">
+            <span style="color:#757575; font-size:16px;">这里是我的详细内容，不要问，</span>
+          </mu-row>
         </mu-col>
       </mu-row>
       <mu-divider></mu-divider>
 
       <mu-row style="margin-top:1rem; margin-bottom:1rem;">
         <mu-col span="3">
-          <mu-flex style="width:100%; height:4rem; background:red; color:#fff; border-radius:1rem;" align-items="center" justify-content="center">
-            <span>公告</span>
+          <mu-flex style="width:100%; height:auto; color:#fff;" align-items="center" >
+              <div style="width:3.8rem; height:3.8rem; background:#4caf50; color:#fff; padding:.5rem; border-radius:50%;">
+                <div style="text-align:center;">
+                  <svg-icon icon-class="notify_activity" class="icon-class"></svg-icon>
+                </div>
+                <div style="text-align:center; font-size:13px;">活 动</div>
+              </div>
           </mu-flex>
         </mu-col>
-        <mu-col span="9" style-"padding:0 0 0 .5rem;">
-          <mu-row style="margin-bottom:.5rem;">标题一</mu-row>
-          <mu-row>这里是我的详细内容，不要问，不要说，就静静的听我说，我真的是</mu-row>
+        <mu-col span="9" align-self="center">
+          <mu-row>
+            <span style="font-size:18px; font-weight:700;">组队成功</span> 
+            <span style="margin-left:auto; font-size:14px; color:#9e9e9e; margin-right:.5rem;">10分钟前</span>
+          </mu-row>
+          <mu-row style="margin-top:.2rem;">
+            <span style="color:#757575; font-size:16px;">这里是我的详细内容，不要问，</span>
+          </mu-row>
         </mu-col>
       </mu-row>
       <mu-divider></mu-divider>
 
       <mu-row style="margin-top:1rem; margin-bottom:1rem;">
         <mu-col span="3">
-          <mu-flex style="width:100%; height:4rem; background:red; color:#fff; border-radius:1rem;" align-items="center" justify-content="center">
-            <span>最新活动</span>
+          <mu-flex style="width:100%; height:auto; color:#fff;" align-items="center" >
+              <div style="width:3.8rem; height:3.8rem; background:#673ab7; color:#fff; padding:.5rem; border-radius:50%;">
+                <div style="text-align:center;">
+                  <svg-icon icon-class="notify_annoucement" class="icon-class"></svg-icon>
+                </div>
+                <div style="text-align:center; font-size:13px;">公 告</div>
+              </div>
           </mu-flex>
         </mu-col>
-        <mu-col span="9">
-          <mu-row>标题一</mu-row>
-          <mu-row>这里是我的详细内容，不要问，不要说，就静静的听我说，我真的是</mu-row>
+        <mu-col span="9" align-self="center">
+          <mu-row>
+            <span style="font-size:18px; font-weight:700;">组队成功</span> 
+            <span style="margin-left:auto; font-size:14px; color:#9e9e9e; margin-right:.5rem;">10分钟前</span>
+          </mu-row>
+          <mu-row style="margin-top:.2rem;">
+            <span style="color:#757575; font-size:16px;">这里是我的详细内容，不要问，</span>
+          </mu-row>
         </mu-col>
       </mu-row>
       <mu-divider></mu-divider>
 
     </div>
 
+    <div class="footer">
+      <Footer ref="footer"></Footer>
+    </div>
+
   </div>
 </template>
 
 <script>
+import Footer from '@/components/Footer.vue'
 export default {
-  
+  mounted () {
+    this.$refs.footer.linkToNav(2)
+  },
+  components: {
+    Footer,
+  },
 }
 </script>
 
 <style scoped>
 .mine-appbar { width: 100%; height:2.5rem; }
+.icon-class {font-size:22px;}
+.footer{width:100%; position:fixed; bottom:0;}
 </style>
 
 
