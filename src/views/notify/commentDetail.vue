80977ef7f1914da6739fe104b6e536d0690c1896
e <2485450596@qq.com>
2019-08-12 15:20:31 +0800

    something new

--- src/views/notify/commentDetail.vue
+++ src/views/notify/commentDetail.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <div style="padding:.5rem;">
+
+    <mu-flex align-items="center" style="width:100%; padding:1rem 1rem .5rem 1rem; margin-bottom:0rem;">
+      <svg-icon icon-class="detail" class="icon-class"></svg-icon>
+      <span style="margin-left:.5rem; font-size:14px; font-weight:700; color:#757575; letter-spacing:1px;">回复详情</span>
+    </mu-flex>
+
+    <mu-divider></mu-divider>
+
+    <div style="padding:1rem .5rem;">
       <mu-flex align-items="center">
         <mu-avatar size="35" :class="Info.sex == 1 ? 'avatar-male' : 'avatar-female'">
           <img :src="Info.avatar | imgPrefixDeal()">
@@ -23,13 +31,25 @@
 
       <mu-row class="create-time">
         <span style="color:#757575;">{{ Info.createTime | formatTime('{y}/{m}/{d} {h}:{i}') }} ·</span>  
-        <span @click="newChat(Info.userID, Info.chatID, Info.nickname)" class="reply-button"> 回复</span>
+        <span @click="newChat(true, Info.userID, Info.chatID, Info.nickname)" class="reply-button"> 回复</span>
       </mu-row>
 
       <mu-row v-if="Info.replyCont" class="reply-origin-row">
         <span><span style="color:#795548;">你：</span>{{ Info.replyCont }}</span>
       </mu-row>
     </div>
+
+    <mu-flex align-items="center" justify-content="between" style="position:fixed; bottom:0; left:0; width:100%; margin-top:1.5rem; padding:.5rem; background:#eeeeee;">
+      <mu-button @click="linkToDetail" style="width:73%; " color="#00bcd4">
+        <span style="letter-spacing:1px;">{{ ButtonDesc }}</span>
+      </mu-button>
+
+      <mu-button style="width:20%;" color="success" @click="newChat(true, Info.userID, Info.chatID, Info.nickname)">
+        <span style="letter-spacing:1px;">回复</span>
+      </mu-button>
+
+    </mu-flex>
+
   </div>
 </template>
 
@@ -38,6 +58,9 @@ export default {
   data () {
     return {
       NotifyID: 0,
+      TeamType: 0,
+      TeamID: 0,
+      ButtonDesc: '',
       Info: {}
     }
   },
@@ -50,17 +73,70 @@ export default {
       this.$axios.get(`/notify/chatDetail/${this.NotifyID}`, {}).then((resp)=>{
         if(resp.data.code == 20000) {
           this.Info = resp.data.msg
+          this.TeamType = this.Info.teamType
+          this.TeamID = this.Info.teamID
+          switch(this.TeamType) {
+          case 1:
+            this.ButtonDesc = '跳转到该游戏组队'
+            break
+          case 2:
+            this.ButtonDesc = '跳转到该聚会组队'
+            break
+          case 3:
+            this.ButtonDesc = '跳转到该旅游组队'
+            break
+          case 4:
+            this.ButtonDesc = '跳转到该即时组队'
+            break
+          case 5:
+            this.ButtonDesc = '跳转到该活动组队'
+            break
+          case 6:
+            this.ButtonDesc = '跳转到该文章详情'
+            break
+          case 7:
+            this.ButtonDesc = '跳转到该游戏评论'
+            break
+          }
         }else{
           this.$toast.message('发生未知错误')
         }
-        console.log(resp)
       })
+    },
+    newChat (isReply, replyTo, replyID, replyNickname) {
+      this.$router.push({path:`/common/newChat`, query:{teamType:this.TeamType, teamID:this.TeamID, isReply, replyTo, replyID, replyNickname}})
+    },
+    linkToDetail () {
+      switch(this.TeamType) {
+      case 1: // 游戏模块
+        this.$router.push(`/game/detail/${this.TeamID}`)
+        break 
+      case 2: // party
+        this.$router.push(`/party/teamDetail/${this.TeamID}`)
+        break
+      case 3: // travel
+        this.$router.push(`/travel/teamDetail/${this.TeamID}`)
+        break
+      case 4: // instant
+        this.$router.push(`/instant/detail/${this.TeamID}`)
+        break
+      case 5: // activity
+        this.$router.push(`/activity/detail/${this.TeamID}`)
+        break
+      case 6: // article
+        this.$router.push(`/article/detail/${this.TeamID}`)
+        break
+      case 7: // 游戏评论
+        this.$router.push(`/game/commentDetail/${this.TeamID}`)
+        break
+      }
     }
   }
 }
 </script>
 
 <style scope>
+.icon-class {font-size:17px;}
 .chat-container { padding:.5rem .5rem; }
 .avatar-male { padding:.1rem; border:1px solid #03a9f4; border-radius:50%; background:white; }
 .avatar-female { padding:.1rem; border:1px solid #e91e63; border-radius:50%; background:white; }
