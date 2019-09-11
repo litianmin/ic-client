8dff7b6232135cacd88e9ee6c6bc7a7ae19b2ef3
e <2485450596@qq.com>
2019-09-11 17:59:38 +0800

    new

--- src/views/travel/newTeam.vue
+++ src/views/travel/newTeam.vue
@@ -17,8 +17,8 @@
       <!-- 心灵、朝圣、亲子、摄影、漂流、美食、滑雪、沙滩、踏青 -->
       <!-- 旅游主题 -->
       <mu-flex style="width:100%; margin-bottom:1rem;" align-items="center">
-        <span style="font-size:12px; color:#795548; margin-right:.5rem;">旅游主题：</span>
-        <select v-model="TravelTheme" name="partyTitle" id="" style="padding:.1rem .5rem; color:#009688; appearance:none; background:#fff; font-size:12px; border-radius:.2rem; border:1px solid #80cbc4; ">
+        <span style="font-size:13px; color:#795548; margin-right:.5rem;">旅游主题：</span>
+        <select v-model="TravelTheme" name="partyTitle" id="" style="padding:.1rem .5rem; color:#424242; appearance:none; background:#fff; font-size:12px; border-radius:.2rem; border:1px solid #bdbdbd; ">
           <option value="1">心 灵</option>
           <option value="2">漂 流</option>
           <option value="3">摄 影</option>
@@ -33,14 +33,14 @@
 
       <!-- 旅游路程、形式 -->
       <mu-flex style="width:100%; margin-bottom:1rem;" align-items="center">
-        <span style="font-size:12px; color:#795548; margin-right:.5rem;">旅游路程：</span>
-        <select v-model="PathLength" name="partyTitle" id="" style="padding:.1rem .5rem; color:#009688; appearance:none; background:#fff; font-size:12px; border-radius:.2rem; border:1px solid #80cbc4; ">
+        <span style="font-size:13px; color:#795548; margin-right:.5rem;">旅游路程：</span>
+        <select v-model="PathLength" name="partyTitle" id="" style="padding:.1rem .5rem; color:#424242; appearance:none; background:#fff; font-size:12px; border-radius:.2rem; border:1px solid #bdbdbd; ">
           <option value="1">周 边 游</option>
           <option value="2">长途旅行</option>
         </select>
 
-        <span style="font-size:12px; color:#795548; margin-left:2.5rem; margin-right:.5rem;">旅游形式：</span>
-        <select v-model="TravelType" name="partyTitle" id="" style="padding:.1rem .5rem; color:#009688; appearance:none; background:#fff; font-size:12px; border-radius:.2rem; border:1px solid #80cbc4; ">
+        <span style="font-size:13px; color:#795548; margin-left:2.5rem; margin-right:.5rem;">旅游形式：</span>
+        <select v-model="TravelType" name="partyTitle" id="" style="padding:.1rem .5rem; color:#424242; appearance:none; background:#fff; font-size:12px; border-radius:.2rem; border:1px solid #bdbdbd; ">
           <option value="1">自 由 行</option>
           <option value="2">跟 团 游</option>
         </select>
@@ -48,21 +48,21 @@
 
       <!-- 招募标题 -->
       <mu-flex style="width:100%; margin-bottom:1rem;" align-items="center">
-        <span style="font-size:12px; color:#795548; margin-right:.5rem;">招募标题：</span>
-        <input maxlength="15" v-model="TravelTitle" style="padding:.4rem; width:70%; font-size:12px; border-radius:.2rem; border:1px solid #80cbc4; color:#212121;" type="text" placeholder="一起来玩吧！（不超过15个字符）">
+        <span style="font-size:13px; color:#795548; margin-right:.5rem;">招募标题：</span>
+        <input maxlength="15" v-model="TravelTitle" style="padding:.4rem; width:70%; font-size:12px; border-radius:.2rem; border:1px solid #bdbdbd; color:#212121;" type="text" placeholder="一起来玩吧！（不超过15个字符）">
       </mu-flex>
 
       <!-- 招募人数 -->
       <mu-flex style="width:100%; margin-bottom:1rem;" align-items="center">
-        <span style="font-size:12px; color:#795548; margin-right:.5rem;">招募人数：</span>
-        <select v-model="RecruitNumb" name="" id="" style="padding:.1rem .5rem; color:#009688; appearance:none; background:#fff; font-size:12px; border-radius:.2rem; border:1px solid #80cbc4; text-align:center;">
+        <span style="font-size:13px; color:#795548; margin-right:.5rem;">招募人数：</span>
+        <select v-model="RecruitNumb" name="" id="" style="padding:.1rem .5rem; color:#424242; appearance:none; background:#fff; font-size:12px; border-radius:.2rem; border:1px solid #bdbdbd; text-align:center;">
           <option v-for="(item, index) in RecruitNumbList" :key="index" :value="item">{{ item }} 人</option>
         </select>
       </mu-flex>
 
       <!-- 集合地点 -->
       <mu-flex style="width:100%; margin-bottom:1rem; margin-top:1rem;" align-items="center">
-        <span style="font-size:12px; color:#795548; margin-right:.5rem;">集合地点：</span>
+        <span style="font-size:13px; color:#795548; margin-right:.5rem;">集合地点：</span>
         <span style="font-size:12px; color:#9e9e9e;">{{ MeetingVenue }}</span>
         <mu-icon @click="chooseMeetingVenue" value="person_pin_circle" size="20" color="#009688"></mu-icon>
       </mu-flex>
@@ -72,16 +72,27 @@
 
       <!-- 详细说明 -->
       <mu-flex style="width:100%; margin-bottom:1rem;" align-items="start">
-        <span style="font-size:12px; color:#795548; margin-right:.5rem;">详细说明：</span>
-        <textarea v-model="TravelDetail" style="width:75%; height:7rem; border:1px solid #80cbc4; color:#212121; font-size:12px; padding:.4rem; border-radius:.2rem;" placeholder="大家来这里一起玩吧，出来走走也好！（活动的详细内容）">
+        <span style="font-size:13px; color:#795548; margin-right:.5rem;">详细说明：</span>
+        <textarea v-model="TravelDetail" style="width:75%; height:6rem; border:1px solid #bdbdbd; color:#212121; font-size:12px; padding:.4rem; border-radius:.2rem;" placeholder="大家来这里一起玩吧，出来走走也好！（活动的详细内容）">
         </textarea>
+      </mu-flex>
+
+      <!-- 招募图片 -->
+      <mu-flex style="width:100%; margin-bottom:.5rem;" align-items="start">
+        <span style="font-size:13px; color:#795548; margin-right:.5rem; margin-top:.3rem;">招募图片：</span>
+        <ImgCropper 
+          @getImgURL="getRecruitImg" 
+          @delImg="delRecruitImg"
+          ImgWidth="177px" 
+          ImgHeight="100px" 
+          BorderColor="#e0e0e0"></ImgCropper>
       </mu-flex>
 
     </mu-container>
 
     <!-- 路线、活动内容 -->
-    <mu-container style="padding:0 .8rem 0 0; margin-bottom:1rem;">
-      <span style="font-size:12px; color:#795548; margin-right:.5rem; margin-left:.5rem; ">路线/活动内容：</span>
+    <mu-container style="padding:0 .8rem 0 0; margin-bottom:4rem;">
+      <span style="font-size:13px; color:#795548; margin-right:.5rem; margin-left:.5rem; ">路线/活动内容：</span>
       <div class="demo-vsteper-container">
         <mu-stepper :active-step="VactiveStep" orientation="vertical" style="margin-bottom:1rem;">
 
@@ -93,12 +104,12 @@
 
               <mu-flex style="width:100%; margin-bottom:.5rem; margin-top:.5rem;" align-items="center">
                 <span style="font-size:12px; color:#795548; margin-right:.5rem;">地点/活动：</span>
-                  <input maxlength="15" v-model="item.addrOrProgram" style="padding:.4rem; width:70%; font-size:12px; border-radius:.2rem; border:1px solid #e0e0e0; color:#212121;" type="text" placeholder="（不超过20个字符）">
+                <input maxlength="15" v-model="item.addrOrProgram" style="padding:.4rem; width:70%; font-size:12px; border-radius:.2rem; border:1px solid #e0e0e0; color:#212121;" type="text" placeholder="（不超过20个字符）">
               </mu-flex>
 
               <mu-row style="width:100%; margin-top:.5rem;" align-items="end">
-                  <mu-date-input v-model="item.beginTime" type="dateTime"  prefix="开始时间：" style="font-size:12px;" landscape container="bottomSheet" clock-type="24hr" view-type="clock" actions ></mu-date-input>
-                  <mu-date-input v-model="item.endTime" type="dateTime" prefix="结束时间：" style="font-size:12px; margin-top:-1rem;" landscape container="bottomSheet" clock-type="24hr" view-type="clock" actions></mu-date-input>
+                <mu-date-input v-model="item.beginTime" type="dateTime"  prefix="开始时间：" style="font-size:12px;" landscape container="bottomSheet" clock-type="24hr" view-type="clock" actions ></mu-date-input>
+                <mu-date-input v-model="item.endTime" type="dateTime" prefix="结束时间：" style="font-size:12px; margin-top:-1rem;" landscape container="bottomSheet" clock-type="24hr" view-type="clock" actions></mu-date-input>
               </mu-row>
 
               <mu-flex style="width:100%; margin-bottom:1rem; margin-top:-.5rem;" align-items="start">
@@ -108,9 +119,9 @@
 
               <!-- 添加图片 -->
               <mu-flex style="width:50%; height:4rem; padding:.5rem; position:relative; border:1px solid #e0e0e0; border-radius:.3rem;" justify-content="center" align-items="center">
-                  <mu-icon v-show="item.descImg.length > 0" @click="delImg(index)" value="cancel" color="#f06292" style="position:absolute; top:-.8rem; right:-.8rem;"></mu-icon>
-                  <mu-icon @click="addImg(index)" v-show="item.descImg.length == 0" value="add" size="40" color="#e0e0e0" style=""></mu-icon>
-                  <img style="max-width:100%; max-height:4rem; border-radius:.3rem;" :src="item.descImg" alt="">
+                <mu-icon v-show="item.descImg.length > 0" @click="delImg(index)" value="cancel" color="#f06292" style="position:absolute; top:-.8rem; right:-.8rem;"></mu-icon>
+                <mu-icon @click="addImg(index)" v-show="item.descImg.length == 0" value="add" size="40" color="#e0e0e0" style=""></mu-icon>
+                <img style="max-width:100%; max-height:4rem; border-radius:.3rem;" :src="item.descImg" alt="">
               </mu-flex>
 
               <mu-button v-if="index > 0" class="demo-step-button" @click="vhandlePrev" color="#00bcd4" small>上一个</mu-button>
@@ -128,15 +139,15 @@
             已完成！<span style="color:#009688;" @click="vreset">点击重置</span>
           </span>
         </mu-flex>
-        
-        <!-- 提交按钮 -->
-        <mu-flex align-items="center" justify-content="center" style=" width:100%; margin-top:1.5rem;">
-          <mu-button @click="submit" style="width:100%;" color="#03a9f4">
-            发起组队
-          </mu-button>
-        </mu-flex>
       </div>
     </mu-container>
+
+    <mu-flex style="position:fixed; width:100%; bottom:0; left:0; background:#f5f5f5; padding:.5rem .1rem; z-index:999; border-top:1px solid #eeeeee;" justify-content="center">
+      <mu-button @click="submit" style="width:95%;" color="#00bcd4">
+        <mu-icon value="touch_app" size="14"></mu-icon>
+        <span style="margin-left:.5rem;">发起组队</span>
+      </mu-button>
+    </mu-flex>
 
     <!-- BEGIN 地图弹出框 -->
     <div v-show="AddrChooseWindowIsShow" id="iframe" style="position:fixed; top:0; width:100%; height:100%;">
@@ -153,7 +164,12 @@
 </template>
 
 <script>
+import ImgCropper from '@/components/ImgCropper'
+import { imgCompress } from '@/common/imgDeal.js'
 export default {
+  components: {
+    ImgCropper
+  },
   data () {
     return {
       VactiveStep: 0,
@@ -165,6 +181,7 @@ export default {
       RecruitNumb: 1, // 招募人数
       RecruitNumbList: [],  // 招募人数的列表
       MeetingVenue: '', // 聚会地点
+      RecruitImg: '',
       MeetingVenueObj: {
         name: '',
         lng: 0,
@@ -173,7 +190,7 @@ export default {
       },
       MeetingTime: new Date, // 活动开始时间
       StepList: [
-        {addrOrProgram: '', beginTime: undefined, endTime: undefined, travelDesc: '', descImg: ''},
+        {addrOrProgram: '', beginTime: undefined, endTime: undefined, travelDesc: '', descImg: '', descImgURL: ''},
       ],
       HadAddImg: 0,
       ClickAddImgIndex: 0,
@@ -200,7 +217,7 @@ export default {
 
       // 往StepList添加数据
       if(index == this.StepList.length - 1) {
-        let item = {addrOrProgram: '', beginTime: undefined, endTime: undefined, travelDesc: '', descImg: ''}
+        let item = {addrOrProgram: '', beginTime: undefined, endTime: undefined, travelDesc: '', descImg: '', descImgURL: ''}
         this.StepList.push(item)
       }
       this.VactiveStep++
@@ -214,6 +231,15 @@ export default {
     vhandleFinished () {
       this.VactiveStep = this.StepList.length + 1
     },
+
+    getRecruitImg (imgURL) {  // 获取招募图片
+      this.RecruitImg = imgURL
+    },
+
+    delRecruitImg () {  // 删除招募图片
+      this.RecruitImg = ''
+    },
+
     addImg (index) {
       if(this.HadAddImg > 2) {
         this.$toast.message('路线/地点列表最多只能添加三张图片')
@@ -223,22 +249,17 @@ export default {
       this.$refs.imgUpload.click()
     },
     getImgList () {  // 获取评论图片
-      var _this = this
-      var event = event || window.event
-      var file = event.target.files[0]
-      if(file.size > 1024 * 1024 * 2) {
-        this.$toast.message('图片上传最大为2M')
-        return
-      }
-      var reader = new FileReader()
-      //转base64
-      reader.onload = function(e) {
-        let imgIndex = _this.ClickAddImgIndex
-        _this.StepList[imgIndex].descImg = e.target.result
-        _this.HadAddImg++
-      }
-      reader.readAsDataURL(file)
+      let event = event || window.event
+      let file = event.target.files[0]
+      imgCompress(file, this.imgHadChoose, 'travel', this.ClickAddImgIndex) // 图片处理
     },
+
+    imgHadChoose (res, index, imgURL) {
+      this.StepList[index].descImg = res
+      this.StepList[index].descImgURL = imgURL
+      this.HadAddImg++
+    },
+
     delImg (index) {
       this.StepList[index].descImg = ''
       this.HadAddImg--
@@ -320,7 +341,8 @@ export default {
         if(i == (stepList.length - 1) && stepList[i].addrOrProgram == '') {
           stepList.pop()
         }
-
+        stepList[i].descImg = stepList[i].descImgURL
+        delete stepList[i].descImgURL
       }
 
       // 开始提交数据
