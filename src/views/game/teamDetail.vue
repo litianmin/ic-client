dba2b79eb5fb6a91357a0e2376f73d3fe53ee12a
“litianmin <l>
2019-11-22 23:26:57 +0800

    new

--- src/views/game/teamDetail.vue
+++ src/views/game/teamDetail.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <!-- BEGIN 头部 -->
-
     <mu-flex 
       class="gb-top-bar" 
       align-items="center">
@@ -9,7 +8,7 @@
         <svg-icon icon-class="goback" style="font-size:20px;"></svg-icon>
       </mu-flex>
       <mu-flex align-items="center" style="padding: 0 0 0 2rem;">
-        <span style="color:#fff;">{{ ChatDetailMain.captain_nickname }} 的队伍</span>
+        <span style="color:#fff;">{{ MainInfo.nickname }} 的队伍</span>
       </mu-flex>
 
       <mu-flex style="margin-left:auto; margin-right:.5rem;">
@@ -21,30 +20,24 @@
     <!-- END 头部 -->
 
     <div style="margin-bottom:3rem; margin-top:2.5rem;">  
-      <!-- BEGIN 主评论 -->
       <mu-container class="main-team-container" style="position:relative;">
         <mu-flex align-items="center">
-          <mu-avatar size="30" @click="$router.push(`/usr/usercard/${ChatDetailMain.userID}`)">
-            <img :src="ChatDetailMain.captain_avatar | imgPrefixDeal()">
+          <mu-avatar size="30" @click="$router.push(`/usr/usercard/${MainInfo.sponser}`)">
+            <img :src="MainInfo.avatar | imgPrefixDeal()">
           </mu-avatar>
           <span class="team-item-nickname">
-            {{ ChatDetailMain.captain_nickname }} <span class="character-title">( 队长 )</span>
+            {{ MainInfo.nickname }} <span class="character-title">( 队长 )</span>
           </span>
-          <!-- <span class="reply-time">10小时前</span> -->
+          <span class="reply-time">{{ MainInfo.createTime | formatTime('{y}-{m}-{d} {h}:{i}') }}</span>
         </mu-flex>
 
         <mu-row class="team-item-text">
-          {{ ChatDetailMain.recruit_word }}
+          {{ MainInfo.recruitWord }}
         </mu-row>
 
-        <mu-row v-if="ChatDetailMain.recruit_img" class="team-item-img">
-          <img :src="ChatDetailMain.recruit_img | imgPrefixDeal()" alt="">
+        <mu-row v-if="MainInfo.recruitImg" class="team-item-img">
+          <img :src="MainInfo.recruitImg | imgPrefixDeal()" alt="">
         </mu-row>
-
-        <svg-icon v-if="TeamStmt == 0" icon-class="recruiting" style="opacity: .5; position:absolute; right:.5rem; top:0; font-size:60px;"></svg-icon>
-        <svg-icon v-if="TeamStmt == 1" icon-class="recruit_finished" style="opacity: .5; position:absolute; right:.5rem; top:0; font-size:60px;"></svg-icon>
-        <svg-icon v-if="TeamStmt == 2" icon-class="recruit_fail" style="opacity: .5; position:absolute; right:.5rem; top:0; font-size:60px;"></svg-icon>
-        <svg-icon v-if="TeamStmt == 3" icon-class="recruit_delete" style="opacity: .5; position:absolute; right:.5rem; top:0; font-size:60px;"></svg-icon>
 
       </mu-container>
       <!-- END 主评论 -->
@@ -58,95 +51,45 @@
         <input type="text" placeholder="我也来说一句吧" disabled>
       </div>
 
-
-      <span v-if="JointeamStmt == 3" @click="inviteFriend" style="font-size:19px; margin-left:auto;">
+      <!-- 已加入，如果队伍招募中，显示招募按钮 -->
+      <span 
+        v-if="MainInfo.teamStatus == 0 && SelfInfo.joinStatus == 3" 
+        @click="inviteFriend" 
+        style="font-size:19px; margin-left:auto;">
         <svg-icon icon-class="team-invite"></svg-icon>
       </span>
 
-      <span v-if="JointeamStmt == 2" @click="joinTeam(2)" style="font-size:19px; margin-left:auto;">
-        <svg-icon icon-class="hadjointeam"></svg-icon>
-      </span>
-      <span v-if="JointeamStmt == 1" @click="joinTeam(1)" style="font-size:20px; margin-left:auto;">
-        <svg-icon icon-class="jointeam_applying"></svg-icon>
-      </span>
-      <span v-if="JointeamStmt == 0" @click="joinTeam(0)" style="font-size:20px; margin-left:auto;">
+      <!-- 已离队或者未加入组队， 并且队伍招募中 -->
+      <span 
+        v-if="MainInfo.teamStatus == 0 && SelfInfo.joinStatus == 4" 
+        @click="joinTeam" 
+        style="font-size:17px; margin-left:auto; margin-top:.1rem;">
         <svg-icon icon-class="jointeam"></svg-icon>
       </span>
 
-      <mu-icon @click="$refs.shareGuide.show()" value="share" class="reply-input-box-icon" size="18" color="#8A8A8A"></mu-icon>
+      <!-- 队伍已完成，显示完成 -->
+      <span 
+        v-if="MainInfo.teamStatus == 1 " 
+        style="font-size:18px; margin-left:auto; margin-top:.1rem;">
+        <svg-icon icon-class="team-finish"></svg-icon>
+      </span>
+
+      <span 
+        @click="$refs.shareGuide.show()" 
+        style="font-size:20px; margin-left:auto; margin-right:.3rem;">
+        <svg-icon icon-class="share-3d"></svg-icon>
+      </span>
+
     </mu-flex>
-
-    <!-- BEGIN 弹出窗口 -->
-    <mu-container>
-      <mu-drawer 
-        :open.sync="teamListWindowIsShow" 
-        :docked="false" 
-        :left="true" 
-        width="80%">
-        <!-- 游戏基本信息 -->
-        <mu-row style="position:relative;">
-          <img 
-            style="max-width:100%; max-height:100%;" 
-            :src="GameDisplayImg | imgPrefixDeal()" 
-            alt="">
-
-          <div style="position:absolute; left:0; bottom:0; width:100%; padding:.5rem 1rem;  background:rgba(30, 30, 30, .3)">
-            <div style="color:#fff;">{{ GameName }}</div>
-            <div style="font-size:12px; color:#fff;">区服: <span style="margin-left:.5rem; color:#fff;">{{ ChatDetailMain.server_name }}</span></div>
-            <div style="font-size:12px; color:#fff;">招募人数: <span style="margin-left:.5rem; color:#fff;">{{ ChatDetailMain.recruit_num }}/<span style="font-size:8px;">{{ ChatDetailMain.had_join }}</span></span></div>
-            <div style="font-size:12px; color:#fff;">队友偏向: <span style="margin-left:.5rem; color:#fff;">{{ ChatDetailMain.teammate_prefer }}</span></div>
-          </div>
-        </mu-row>
-
-        <!-- 队友列表 -->
-        <div 
-          v-for="(item, index) in TeammateList" 
-          :key="index"
-          style="margin-top:.5rem; padding: 0 .5rem;" >
-          <mu-row class="teammate-box">
-            <mu-col span="8">
-              <mu-flex>
-                <div style="position:relative;">
-                  <mu-avatar size="28" @click="$router.push(`/usr/usercard/${item.user_id}`)">
-                    <img :src="item.user_avatar | imgPrefixDeal()" />
-                  </mu-avatar>
-                </div>
-                <div>
-                  <span class="teammate-info-title">昵称：<span style="color:#9e9e9e;">{{ item.nickname }}</span></span>
-                  <br/>
-                  <span class="teammate-info-title">角色：<span style="color:#9e9e9e;">{{ item.role_name }}</span></span>
-                  <br/>
-                  <span class="teammate-info-title">职业：<span style="color:#9e9e9e;">{{ item.role }}/{{ item.role_rank }}</span></span>
-                </div>
-              </mu-flex>
-            </mu-col>
-
-            <mu-col span="4" justify-content="center">
-              <mu-flex justify-content="center" align-items="center" class="teammate-img-flex">
-                  <img :src="item.display_img | imgPrefixDeal()" />
-              </mu-flex>
-            </mu-col>
-          </mu-row>
-        </div>
-        
-      </mu-drawer>
-    </mu-container>
-    <!-- END 弹出窗口 -->
 
     <!-- 底部弹出框 -->
     <mu-bottom-sheet :open.sync="ExpandBox" :lock-scroll="true">
       <mu-list @item-click="ExpandBox = false">
 
-        <mu-list-item button @click="teamListWindowToggle">
-          <mu-list-item-action>
-            <svg-icon icon-class="leave-team-green" style="font-size:20px;"></svg-icon>
-          </mu-list-item-action>
-          <mu-list-item-title>
-            <span style="color:#424242; letter-spacing:1px;">队友列表</span>
-          </mu-list-item-title>
-        </mu-list-item>
-
-        <mu-list-item button v-if="JointeamStmt == 3" @click="leaveTeam">
+        <mu-list-item 
+          button 
+          v-if="SelfInfo.joinStatus == 3" 
+          @click="leaveTeam">
           <mu-list-item-action>
             <svg-icon icon-class="leave-team-green" style="font-size:20px;"></svg-icon>
           </mu-list-item-action>
@@ -155,7 +98,10 @@
           </mu-list-item-title>
         </mu-list-item>
 
-        <mu-list-item button v-else @click="joinTeam">
+        <mu-list-item 
+          v-if="MainInfo.teamStatus == 0 && SelfInfo.joinStatus == 4" 
+          @click="joinTeam"
+          button>
           <mu-list-item-action>
             <svg-icon icon-class="join-team-green" style="font-size:20px;"></svg-icon>
           </mu-list-item-action>
@@ -164,7 +110,10 @@
           </mu-list-item-title>
         </mu-list-item>
 
-        <mu-list-item button @click="inviteFriend">
+        <mu-list-item 
+          v-if="MainInfo.teamStatus == 0 && SelfInfo.joinStatus == 3" 
+          @click="inviteFriend"
+          button>
           <mu-list-item-action>
             <svg-icon icon-class="team-invite" style="font-size:20px;"></svg-icon>
           </mu-list-item-action>
@@ -186,37 +135,38 @@ import { wxInit } from '@/common/wxInit.js'
 import ChatList from '@/components/ChatList.vue'
 import WxShareGuide from '@/components/WxShareGuide.vue'
 export default {
+  components: {
+    ChatList,
+    WxShareGuide,
+  },
   data () {
     return {
       ShareTitle: '', // 分享title
       ShareDesc: '',  // 分享描述
       ShareImgUrl: '',  // 分享图片
 
-      TeamType: 1,
+      TeamType: 1,  // 固定值，这个应该配置在全局的，后面做
       Title: '队伍招募中。。。',
       TeamID: 0,
       GameID: 0,
-      SelfIsCamptain: false,
-      ChatDetailMain: {
-        userID: 0,
-        recruit_word: '',
-        recruit_img: "",
-        recruit_way: "",
-        captain_avatar: "",
-        captain_nickname: "",
-        captain_sex: 0,
-        server_name: '',
-        recruit_num: 0,
-        had_join: 0,
-        teammate_prefer: '',
+
+      MainInfo: { // 主要的信息
+        sponser: 0, // 发起人用户id
+        avatar: '', 
+        nickname: '',
+        recruitImg: '',
+        recruitWord: '',
+        recruitNumb: 0,
+        teamStatus: 0,  // 队伍当前状态
+        createTime: 0
       },
-      TeammateList: [],
-      teamListWindowIsShow: false,
-      JointeamStmt: 0,
-      JointeamStmtDesc: '',
-      TeamStmt: 0,
-      GameDisplayImg: '',
-      GameName: '',
+
+      SelfInfo: { // 当前用户相关状态
+        isCaptain: false, // 是否为队长
+        joinStatus: 0 // 加入的状态
+      },
+
+      TeammateList: [], // 队友列表
 
       ExpandBox: false,
     }
@@ -232,56 +182,32 @@ export default {
       // 页面初始化
       this.$axios.post(`/game/teamDetail/${this.TeamID}`, {}).then((resp)=>{
 
-        console.log(resp.data)
-
         if(resp.data.code != 20000) {
-          this.$toast.message('该队伍已解散')
+          this.$toast.message(resp.data.msg)
+          this.$router.go(-1)
           return
         }
-        let dataBack = resp.data.msg 
-        this.ChatDetailMain.userID = dataBack.teamDetail.captain_userid
-        this.ChatDetailMain.recruit_word = dataBack.teamDetail.announcement
-        this.ChatDetailMain.recruit_img = dataBack.teamDetail.recruit_img
-        this.ChatDetailMain.recruit_way = dataBack.teamDetail.recruit_way
-        this.ChatDetailMain.captain_avatar = dataBack.teamDetail.captain_avatar
-        this.ChatDetailMain.captain_nickname = dataBack.teamDetail.captain_nickname
-        this.ChatDetailMain.captain_sex = dataBack.teamDetail.captain_sex
-        this.ChatDetailMain.server_name = dataBack.teamDetail.server_name
-        this.ChatDetailMain.recruit_num = dataBack.teamDetail.recruit_num
-        this.ChatDetailMain.had_join = dataBack.teamDetail.TeammateList.length
-        this.ChatDetailMain.teammate_prefer = dataBack.teamDetail.teanmate_prefer
-        this.SelfIsCamptain = dataBack.teamDetail.self_iscaptain == 0 ? false : true
-        this.GameDisplayImg = dataBack.gameInfo.gameDisplayImg
-        this.GameName = dataBack.gameInfo.gameName
-        this.GameID = dataBack.teamDetail.g_id
-        this.JointeamStmt = dataBack.selfJoinStmt
+        let data = resp.data.msg 
 
-        this.TeammateList = dataBack.teamDetail.TeammateList
-        
-        this.TeamStmt = dataBack.teamDetail.t_stmt
-        // 判断队伍的状态，然后再去修改加入队伍的状态
-        if(this.TeamStmt > 0) { // 只有队伍有效的时候，才显示加入，申请中，加入成功的按钮
-          this.JointeamStmt = 3
+        this.MainInfo = { // 主要的信息
+          sponser: data.teammate[0].userID, 
+          avatar: data.teammate[0].avatar, 
+          nickname: data.teammate[0].nickname,
+          recruitImg: data.recruitImg,
+          recruitWord: data.recruitWord,
+          recruitNumb: data.recruitNumb,
+          teamStatus: data.teamStatus,
+          createTime: data.createTime
         }
-        // 组队状态, 0=>组队中， 1=>成功， 2=>失效, 3=>删除
-        switch(this.TeamStmt) {
-          case 0:
-            this.Title = '招募中'
-            break
-          case 1:
-            this.Title = '已失效'
-            break
-          case 2:
-            this.Title = '已删除'
-            break
-          case 3:
-            this.Title = '组队成功'
-            break
+        this.SelfInfo = { 
+          isCaptain: data.isCaptain, 
+          joinStatus: data.selfJoinStatus 
         }
+        this.TeammateList = data.teammate
 
         // 微信分享
-        this.ShareTitle = `${this.GameName}, 有人玩没！`   // 分享title
-        this.ShareDesc = this.ChatDetailMain.recruit_word   // 分享描述
+        this.ShareTitle = `一起玩游戏咯，万年单机，万年单身！`   // 分享title
+        this.ShareDesc = this.MainInfo.recruitWord   // 分享描述
 
       })
     },
@@ -291,39 +217,17 @@ export default {
     },
 
     inviteFriend () { // 邀请好友
-      // 先判断该队伍是否在招募中
-      if (this.TeamStmt != 0) {
-        this.$toast.info('队伍已停止招募，不能邀请好友')
-        return
-      }
-
       this.$router.push(`/usr/inviteFriend/${this.TeamID}/${this.TeamType}`)
     },
 
-    joinTeam (stmt) {
-      switch(stmt) {
-        case 0: // 未加入
-          this.$router.push(`/game/joinTeam/${this.TeamID}`)
-          break
-        case 1: // 申请中
-          // 暂不作处理，因为现在还没有这个业务
-          break
-        case 2: // 拒绝加入
-          // 暂不作处理
-          break
-        case 3: // 已加入，跳转到邀请？
-          this.$toast.message("已加入")
-          break
-        case 4: // 已离队
-          this.$router.push(`/game/joinTeam/${this.TeamID}`)
-          break
-      }
+    joinTeam () {
+      this.$router.push(`/game/joinTeam/${this.TeamID}`)
     },
 
     leaveTeam () {  // 离开组队
       // 先判断是否为队长，如果是队长，提示会解散队伍
       // 并且如果是队长，只有组队中才可以解散
-      if(this.SelfIsCamptain == true) {
+      if(this.SelfInfo.isCaptain == true) {
         this.$confirm('是否解散队伍？').then((resp)=>{
           if(resp.result == true) { // 确定解散队伍
             this.leaveTeamReq()
@@ -337,7 +241,7 @@ export default {
     leaveTeamReq () {
       this.$axios.get(`/common/leaveTeam/${this.TeamType}/${this.TeamID}`, {}).then((resp)=>{
         if(resp.data.code == 20000) {
-          this.$toast.message('已成功退出组队！')
+          this.$toast.success('已成功退出组队！')
           this.$router.push(`/game/detail/${this.GameID}`)
           return
         }
@@ -346,11 +250,17 @@ export default {
 
     newChat (isReply, replyTo, replyID, replyNickname) {
       // 只有加入组队的人才能进行评论
-      if(this.JointeamStmt != 3) {
-        this.$toast.message('加入组队后才能聊天哦')
+      if(this.SelfInfo.joinStatus != 3) {
+        this.$toast.info('加入组队后才能聊天哦')
         return
       }
-      this.$router.push({path:`/common/newChat`, query:{teamType:this.TeamType, teamID:this.TeamID, isReply, replyTo, replyID, replyNickname}})
+      this.$router.push({path:`/common/newChat`, query:{
+        teamType:this.TeamType, 
+        teamID:this.TeamID, 
+        isReply, 
+        replyTo, 
+        replyID, 
+        replyNickname}})
     },
 
     teamListWindowToggle () {
@@ -359,43 +269,69 @@ export default {
     },
 
   },
-  components: {
-    ChatList,
-    WxShareGuide,
-  },
 }
 </script>
 
 <style scoped>
 
-.character-title { margin-left:1rem; font-size:12px; color:#795548; }
+.character-title { 
+  margin-left:1rem; 
+  font-size:12px; 
+  color:#795548; 
+}
 
-.main-team-container { background:#ffffff; padding:1rem .5rem 2rem 1rem;  }
-.sort-bar { background:#eeeeee; font-size:12px; padding:.3rem .5rem; margin-bottom:.5rem;  }
-.sort-bar-svg { margin-left:auto; font-size:18px; margin-right:.3rem; }
+.main-team-container { 
+  background:#ffffff; 
+  padding:1rem .5rem 2rem 1rem;  
+}
 
-.reply-container { padding:.5rem .8rem; border-bottom:.1rem solid #ffffff; }
+.reply-time { 
+  margin-left:auto; 
+  font-size:12px; 
+  color:#9e9e9e; 
+  margin-right:.5rem; 
+}
 
-.reply-nickname { font-size:12px; margin-left:.5rem; margin-right:.5rem; color:#4db6ac; }
-.reply-time { margin-left:auto; font-size:12px; color:#9e9e9e; margin-right:.5rem; }
-.reply-cont-box { font-size:12px; padding:.5rem .5rem .5rem 1rem; }
+.reply-input-box { 
+  position:fixed; 
+  bottom:0; 
+  width:100%; 
+  padding:.5rem; 
+  background:#ffffff; 
+  border-top:1px solid #e0e0e0; 
+}
 
-.reply-input-box { position:fixed; bottom:0; width:100%; padding:.5rem; background:#ffffff; border-top:1px solid #e0e0e0; }
-.reply-input-box input { width:100%; border-radius:.3rem; padding:.3rem; background:#f5f5f5; border:0; font-size:12px; }
-.reply-input-box-span { margin-left:auto; font-size:18px; margin-right:.3rem; }
-.reply-input-box-icon { margin-left:auto; margin-right:.5rem; }
+.reply-input-box input { 
+  width:100%; 
+  border-radius:.3rem; 
+  padding:.3rem; 
+  background:#f5f5f5; 
+  border:0; 
+  font-size:12px; 
+}
 
-.team-item-nickname { margin-left:.5rem; font-size:14px; font-weight:600; color:#4db6ac; }
-.team-item-text { padding:.5rem 0 .5rem 1.5rem; font-size:14px; }
-.team-item-img { padding:.5rem .5rem 0 1rem; font-size:12px; color:#616161; }
-.team-item-img img { max-width:100%; max-height:100%; border-radius:.3rem; }
+.team-item-nickname { 
+  margin-left:.5rem; 
+  font-size:14px; 
+  font-weight:600; 
+  color:#4db6ac; 
+}
 
-/* .teammate-box { margin-bottom:.5rem; background:rgba(240, 240, 240, .3); padding:.4rem 0 .4rem .5rem; border-radius:.5rem; border-bottom:1px #bdbdbd solid; border-right:1px #e0e0e0 solid; border-top:1px #f5f5f5 solid; } */
-.teammate-box { margin-bottom:.5rem; background:rgba(240, 240, 240, .3); padding:.4rem 0 .4rem .5rem; border-radius:.5rem; box-shadow: 0px 0px 1px gray; }
-.teammate-info-title { margin-left:.5rem; font-size:12px; color:#795548; }
-.teammate-img-flex { width:100%; height:4rem; text-align:center; border-radius:.5rem; }
-.teammate-img-flex img { max-width:4rem; max-height:4rem; border-radius:.2rem; }
+.team-item-text { 
+  padding:.5rem 0 .5rem 1.5rem; 
+  font-size:14px; 
+}
 
-.friendstmt-flex-one { width:100%; font-size:12px; background:#00bcd4; color:#fff; border-radius:.2rem; margin-right:.3rem; margin-top:.3rem; }
-.friendstmt-flex-two { width:100%; font-size:12px; background:#bdbdbd; color:#fff; border-radius:.2rem; margin-right:.3rem; margin-top:.3rem; }
+.team-item-img { 
+  padding:.5rem .5rem 0 1rem; 
+  font-size:12px; 
+  color:#616161; 
+}
+
+.team-item-img img { 
+  max-width:100%; 
+  max-height:100%; 
+  border-radius:.3rem; 
+}
+
 </style>
