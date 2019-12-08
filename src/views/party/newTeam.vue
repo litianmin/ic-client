c9596222598f7b56ccf6985d550e353871f9ef2e
“yolitianmin <2485450596@qq.com>
2019-12-08 13:55:35 +0800

    new

--- src/views/party/newTeam.vue
+++ src/views/party/newTeam.vue
@@ -18,7 +18,7 @@
       <mu-flex class="theme-choose-flex" align-items="center">
         <span class="theme-title">活动主题：</span>
         <select 
-          v-model="PartyTheme" 
+          v-model="Theme" 
           name="partyTitle" 
           class="select-style">
           <option :value="1">普通聚会</option>
@@ -33,26 +33,22 @@
         </select>
       </mu-flex>
 
-      <mu-flex 
-        class="title-flex" 
-        align-items="start">
-
-        <span class="title-span">聚会标题：</span>
-
-        <textarea 
-          v-model="PartyTitle" 
-          class="title-input" 
-          placeholder="简单明了的标题更容易招募到伙伴噢，限50个字符">
-        </textarea>
-
+      <mu-flex class="meetingsite-container" align-items="center">
+        <span class="title-common">聚会地点：</span>
+        <span class="site-span">{{ PartyVenue }}</span>
+        <mu-icon 
+          @click="AddrChooseWindowIsShow = true" 
+          value="person_pin_circle" 
+          size="20" 
+          color="#009688"></mu-icon>
       </mu-flex>
 
       <mu-flex class="detail-container" align-items="start">
-        <span class="detail-title">详细内容：</span>
+        <span class="detail-title">聚会内容：</span>
         <textarea 
-          v-model="PartyDetail" 
+          v-model="Cont" 
           class="detail-input" 
-          placeholder="请填写下聚会的详细内容吧，限200个字符">
+          placeholder="请填写下聚会的内容，招募你的伙伴吧！(限200个字符)">
         </textarea>
       </mu-flex>
 
@@ -68,26 +64,6 @@
             :key="index" 
             :value="item.val">{{ item.name }}</option>
         </select>
-      </mu-flex>
-
-      <mu-flex class="flex-common" align-items="center">
-        <span class="title-common">活动地点：</span>
-        <span class="site-span">{{ PartyVenue }}</span>
-        <mu-icon 
-          @click="choosePartyAddr" 
-          value="person_pin_circle" 
-          size="20" 
-          color="#009688"></mu-icon>
-      </mu-flex>
-
-      <mu-flex class="meetingsite-container" align-items="center">
-        <span class="title-common">集合地点：</span>
-        <span class="site-span">{{ MeetingVenue }}</span>
-        <mu-icon 
-          @click="chooseMeetingVenue" 
-          value="person_pin_circle" 
-          size="20" 
-          color="#009688"></mu-icon>
       </mu-flex>
 
       <mu-row style="padding: 0 1.5rem 0 0;">
@@ -110,7 +86,7 @@
 
       <!-- 招募图片 -->
       <mu-flex class="flex-common" align-items="start">
-        <span style="font-size:13px; color:#795548; margin-right:.5rem; margin-top:.3rem;">招募图片：</span>
+        <span class="recruitimg-span">招募图片：</span>
         <ImgCropper 
           @getImgURL="getRecruitImg" 
           @delImg="delRecruitImg"
@@ -121,29 +97,45 @@
 
 
       <!-- 上传展示图片 -->
-      <mu-flex class="flex-common" align-items="center">
+      <mu-flex class="flex-common" align-items="center" style="margin-top:1rem;">
         <span class="title-common">展示图片：（最多三张）</span>
       </mu-flex>
 
-      <mu-flex align-items="center" justify-content="around" style="margin-top:1rem; margin-bottom:3rem; border:1px solid #eeeeee; border-radius:.3rem; padding:1rem .5rem;">
-        <mu-flex v-for="(item, index) in UploadImgList" :key="index" style="width:30%; height:8rem; padding:1rem; position:relative; border:1px solid #e0e0e0; border-radius:.3rem; " justify-content="center" align-items="center">
-            <mu-icon @click="delImg(index)" value="cancel" color="#f06292" style="position:absolute; top:0; right:0;"></mu-icon>
-            <img style="max-width:100%; max-height:100%; border-radius:.3rem;" :src="item" alt="">
+      <mu-flex 
+        align-items="center" 
+        justify-content="around" 
+        class="displayimg-container">
+        <mu-flex 
+          v-for="(item, index) in UploadImgList" 
+          :key="index" 
+          class="displayimg-flex" 
+          justify-content="center" 
+          align-items="center">
+            <mu-icon 
+              @click="delImg(index)" 
+              value="cancel" 
+              color="#f06292" 
+              style="position:absolute; top:0; right:0;"></mu-icon>
+            <img class="displayimg" :src="item" />
         </mu-flex>
-        <mu-flex v-if="CouldAddImg" @click="addImg()" style="width:30%; height:8rem; padding:1rem; position:relative; border:1px solid #e0e0e0; border-radius:.3rem;" justify-content="center" align-items="center">
-            <mu-icon value="add" size="40" color="#e0e0e0" style=""></mu-icon>
+        <mu-flex 
+          v-if="CouldAddImg" 
+          @click="addImg()" 
+          class="displayimg-flex"
+          justify-content="center" 
+          align-items="center">
+          <mu-icon value="add" size="40" color="#e0e0e0" style=""></mu-icon>
         </mu-flex>
-        <input @change="getImgList" ref="imgUpload" type="file" style="display:none;" multiple accept="image/*"/>
+        <input 
+          @change="getImgList" 
+          ref="imgUpload" 
+          type="file" 
+          style="display:none;" 
+          multiple 
+          accept="image/*"/>
       </mu-flex>
 
-      <!-- 提交按钮 -->
-      <!-- <mu-flex align-items="center" justify-content="center" style=" width:100%; margin-top:1.5rem;">
-        <mu-button @click="submit" style="width:100%;" color="#00bcd4">
-          发起组队
-        </mu-button>
-      </mu-flex> -->
-
-      <mu-flex style="position:fixed; width:100%; bottom:0; left:0; background:#f5f5f5; padding:.5rem .1rem; z-index:999; border-top:1px solid #eeeeee;" justify-content="center">
+      <mu-flex class="submit-container" justify-content="center">
         <mu-button @click="submit" style="width:95%;" color="#00bcd4">
           <mu-icon value="touch_app" size="14"></mu-icon>
           <span style="margin-left:.5rem;">发起组队</span>
@@ -153,8 +145,10 @@
     </mu-container>
 
     <!-- BEGIN 地图弹出框 -->
-    <div v-show="AddrChooseWindowIsShow" id="iframe" style="position:fixed; top:0; width:100%; height:100%;">
-      <mu-flex @click="shutdownWindow" style="width:10%; height:2.8rem; z-index:9999; position:fixed; top:0; left:0; background:#F8F8F8; text-align:center; padding: 0 0 0 .5rem;" align-items="center" justify-content="center">
+    <div 
+      v-show="AddrChooseWindowIsShow" 
+      id="iframe" style="position:fixed; z-index:10000; top:0; width:100%; height:100%;">
+      <mu-flex @click="shutdownWindow" class="map-container" align-items="center" justify-content="center">
         <mu-icon value="navigate_before"></mu-icon>
       </mu-flex>
       <iframe 
@@ -166,8 +160,6 @@
       </iframe>
     </div>
     <!-- END 地图弹出框 -->
-
-
 
   </div>
 </template>
@@ -182,22 +174,14 @@ export default {
   },
   data () {
     return {
-      PartyTheme: 1,  // 聚会的主题
-      PartyTitle: '', // 聚会的标题
-      PartyDetail: '',  // 聚会详细内容
-      TeammatePrefer: '', // 队友偏爱
-      RecruitNumb: 0, // 招募人数
-      RecruitNumbList: [
-        {val: 0, name: '不限'}
-      ],  // 招募人数的列表
+      Theme: 1,  // 聚会的主题
       AddrChooseWindowIsShow: false,  // 选择地址的窗口是否打开
       PartyVenue: '',  // 活动地点
       PartyVenueObj: {},
-      MeetingVenue: '', // 聚会地点
-      MeetingVenueObj: {},
-      AddrChooseOperate: 0, // 0=>聚会地点， 1=>集合地点
+      Cont: '',  // 聚会详细内容
+      RecruitNumb: 0, // 招募人数
+      RecruitNumbList: [{val: 0, name: '不限'}],  // 招募人数的列表
       BeginTime: undefined, // 活动开始时间
-      EndTime: undefined, // 活动结束时间
       UploadImgList: [],  // 展示图片
       UploadImgURLList: [], // 展示图片的URL
       CouldAddImg: true,  // 判断是否可以继续添加图片
@@ -223,13 +207,6 @@ export default {
       lat: userAddrInfo.lat,
       addr: userAddrInfo.addr,
     }
-    this.MeetingVenue = userAddrInfo.name
-    this.MeetingVenueObj = {
-      name: userAddrInfo.name,
-      lng: userAddrInfo.lng,
-      lat: userAddrInfo.lat,
-      addr: userAddrInfo.addr,
-    }
   },
   watch: {
     UploadImgList: {
@@ -248,47 +225,30 @@ export default {
       this.$router.go(-1)
     },
 
-    loadiframe() {
+    chooseAddrDeal (e) {  // 选择地址处理
+      let that = this
+
+      if (e.data.location != undefined) {
+        let locationStrArr = e.data.location.split(',')
+        let venueObj = {
+          name: e.data.name,
+          lng: Number(locationStrArr[0]),
+          lat: Number(locationStrArr[1]),
+          addr: e.data.address,
+        }
+        that.PartyVenue = e.data.name
+        that.PartyVenueObj = venueObj
+
+        that.AddrChooseWindowIsShow = false
+      }
+    },
+
+    loadiframe() {  // 加载地图选择框
       let iframe = document.getElementById('getAddress').contentWindow
       iframe.postMessage('hello', 'https://m.amap.com/picker/')
-      window.addEventListener("message", function (e) {
-        if (e.data.command != "COMMAND_GET_TITLE") {
-          if(!!e.data.location === false) {
-            return
-          }
-          let locationStrArr = e.data.location.split(',')
-          let venueObj = {
-            name: e.data.name,
-            lng: Number(locationStrArr[0]),
-            lat: Number(locationStrArr[1]),
-            addr: e.data.address,
-          }
-          if(this.AddrChooseOperate == 0) { 
-            // 聚会地点
-            this.PartyVenue = e.data.name
-            this.PartyVenueObj = venueObj
-          }else{  
-            // 集合地点
-            this.MeetingVenue = e.data.name
-            this.MeetingVenueObj = venueObj
-          }
+      window.addEventListener("message", this.chooseAddrDeal, false)
+    },
 
-          // this.LocateAddr = e.data.name
-          this.AddrChooseWindowIsShow = false
-          //业务代码
-          // this.$toast.message(e.data.name)
-        }
-
-      }.bind(this), false)
-    },
-    choosePartyAddr () { // 点击选择地址的时候，弹出地址选择窗口
-      this.AddrChooseOperate = 0
-      this.AddrChooseWindowIsShow = true
-    },
-    chooseMeetingVenue () { // 集合地点
-      this.AddrChooseOperate = 1
-      this.AddrChooseWindowIsShow = true
-    },
     shutdownWindow () { // 关闭地图窗口
       this.AddrChooseWindowIsShow = false
     },
@@ -304,6 +264,7 @@ export default {
     addImg () {
       this.$refs.imgUpload.click()
     },
+
     getImgList () {  // 获取评论图片
       let event = event || window.event
 
@@ -324,51 +285,55 @@ export default {
       this.UploadImgList.splice(index, 1)
       this.UploadImgURLList.splice(index, 1)
     },
+
     submit () {
-      let partyTheme = Number(this.PartyTheme)  // 主题
-      let partyTitle = this.PartyTitle  // 活动主题
-      let partyDetail = this.PartyDetail   // 聚会详细内容
-      let teammatePrefer = this.TeammatePrefer  // 队友偏爱
-      let recruitNumb = Number(this.RecruitNumb)  //招募的人数
-      let partyVenue  = this.PartyVenueObj // 活动地点
-      let meetingVenue = this.MeetingVenueObj  // 集合地点
-      let partyBeginTime = 0 // 活动开始时间
-      let partyEndTime = 0 // 活动结束时间
+
+      let theme = this.Theme
+      let cont = this.Cont
+      let numb = this.RecruitNumb
+      let site = this.PartyVenueObj
+      let beginTime = this.BeginTime
       let recruitImg = this.RecruitImg
-      let uploadImgList = this.UploadImgURLList  // 招募图片列表
+      let displayImgs = this.UploadImgURLList
 
-      if(!!this.EndTime === true) {
-        partyEndTime = Date.parse(this.EndTime.toString()) / 1000
+      if (cont.length == 0) {
+        this.$toast.info('聚会内容不能为空噢')
+        return
       }
-      partyBeginTime = Date.parse(this.BeginTime.toString()) / 1000
+
+      if (!!beginTime === false) {
+        this.$toast.info('请选择活动开始时间')
+        return
+      }
+
+      if (recruitImg.length == 0) {
+        this.$toast.info('招募图片不能为空哟')
+        return
+      }
+
+      beginTime = Date.parse(beginTime.toString()) / 1000
 
 
-      // console.log(this.MeetingVenueObj)
-      // console.log(this.PartyVenueObj)
 
       this.$axios.post('/party/newTeam', {
-          partyTheme,
-          partyTitle,
-          partyDetail,
-          teammatePrefer,
-          recruitNumb,
-          partyVenue,
-          meetingVenue,
-          partyBeginTime,
-          partyEndTime,
-          recruitImg,
-          uploadImgList,
+          theme,
+          cont,
+          numb,
+          site,
+          beginTime,
+          recruitImg, 
+          displayImgs
         }
       ).then((resp)=>{
+
         if(resp.data.code != 20000) {
-          this.$toast.message(resp.data.msg)
+          this.$toast.info(resp.data.msg)
           return
         }
         
         let newTeamID = resp.data.msg
         this.$toast.success('创建成功') // 这里应该跳转到组队详情了
         this.$router.replace(`/party/teamDetail/${newTeamID}`)
-
       })
 
     },
@@ -402,29 +367,6 @@ export default {
   font-size:13px; 
   border-radius:.2rem; 
   border:1px solid #bdbdbd;
-}
-
-.title-flex {
-  width:100%; 
-  margin-bottom:1rem;
-}
-
-.title-span {
-  font-size:13px; 
-  color:#795548; 
-  margin-right:.5rem; 
-  margin-top:.3rem;
-}
-
-.title-input {
-  width:75%; 
-  height:3.5rem; 
-  border:1px solid #bdbdbd; 
-  color:#212121; 
-  font-size:13px; 
-  padding:.4rem; 
-  border-radius:.2rem;
-  letter-spacing: 1px;
 }
 
 .detail-container {
@@ -486,6 +428,59 @@ export default {
   width:100%; 
   margin-bottom:1rem; 
   margin-top:1rem;
+}
+
+.recruitimg-span {
+  font-size:13px; 
+  color:#795548; 
+  margin-right:.5rem; 
+  margin-top:.3rem;
+}
+
+.displayimg-container {
+  margin-top:1rem; 
+  margin-bottom:3rem; 
+  border:1px solid #eeeeee; 
+  border-radius:.3rem; 
+  padding:1rem .5rem;
+}
+
+.displayimg-flex {
+  width:30%; 
+  height:6rem; 
+  padding:1rem; 
+  position:relative; 
+  border:1px solid #e0e0e0; 
+  border-radius:.3rem;
+}
+
+.displayimg {
+  max-width:100%; 
+  max-height:100%; 
+  border-radius:.3rem;
+}
+
+.submit-container {
+  position:fixed; 
+  width:100%; 
+  bottom:0; 
+  left:0; 
+  background:#f5f5f5; 
+  padding:.5rem .1rem; 
+  z-index:999; 
+  border-top:1px solid #eeeeee;
+}
+
+.map-container {
+  width:10%; 
+  height:2.8rem; 
+  z-index:9999; 
+  position:fixed; 
+  top:0; 
+  left:0; 
+  background:#F8F8F8; 
+  text-align:center; 
+  padding: 0 0 0 .5rem;
 }
 
 .map-item { position: fixed; width: 100%; height: 100%; top: 0; background: #fff; }
